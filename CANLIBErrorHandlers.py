c4bcdd6990ca4f4c3b27f396d8c97cfc8ff9645e
bpowell <bpowell@dynamiccontrols.com>
2009-11-09 08:21:55 +1300

    Corrected formatting according to PEP-8 Changed "import *" statements to import the specific functions required

--- CANLIBErrorHandlers.py
+++ CANLIBErrorHandlers.py
@@ -1,8 +1,9 @@
+from canlib import canGetErrorText
+from canstat import CANSTATUS_SUCCESS
 
-from canlib import *
-from canstat import *
 
 class CANLIBError(Exception):
+
     def __init__(self, function, errorCode, arguments):
         Exception.__init__(self)
         self.errorCode = errorCode
@@ -10,8 +11,10 @@ class CANLIBError(Exception):
         self.arguments = arguments
 
     def __str__(self):
-        return ("CANLIBError: function %s failed - %s (arguments were %s)" % 
-          (self.functionName, GetErrorMessage(self.errorCode), self.arguments))
+        return ("CANLIBError: function %s failed - %s - arguments were %s" %
+          (self.functionName, GetErrorMessage(self.errorCode),
+          self.arguments))
+
 
 def GetErrorMessage(result):
     errmsg = create_string_buffer(128)
@@ -22,32 +25,23 @@ def GetErrorMessage(result):
         retVal = ("%s (code %d)" % (errmsg.value, result.value))
     return retVal
 
-# function to check result of a function returning a CAN handle
-#borrowed from Phillip Dixon's Kvaser Python library
+
 def CheckBusHandleValidity(handle, function, arguments):
-    # abort if handle invalid 
     if not is_handle_valid(handle):
         raise CANLIBError(function, handle, arguments)
-    # else return the handle
     else:
         return handle
 
-# general function to check result of a function returning a CAN status
-#borrowed from Phillip Dixon's Kvaser Python library
+
 def CheckStatus(result, function, arguments):
-    # abort if failure 
     if not CANSTATUS_SUCCESS(result):
         raise CANLIBError(function, result, arguments)
-    # else return the status
     else:
         return result
 
-# function to check result of a CAN read function returning a CAN status
-#borrowed from Phillip Dixon's Kvaser Python library
+
 def CheckStatusRead(result, function, arguments):
-    # abort if failure - for Read functions, no message is not an error 
     if not CANSTATUS_SUCCESS(result) and (result.value != canERR_NOMSG):
         raise CANLIBError(function, result, arguments)
-    # else return the status
     else:
         return result
