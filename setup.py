c714c67e874e1092a1d3d1445effa15d1b2ed61b
bpowell <bpowell@localhost>
2009-11-24 15:04:17 +1300

    Added example logger script to setup.py

--- setup.py
+++ setup.py
@@ -20,4 +20,5 @@ if __name__ == "__main__":
         name="pycanlib",
         version=GetVersionNumber(),
         packages=find_packages(exclude=["test"]),
+        scripts=["./bin/can_logger.py"],
     )
