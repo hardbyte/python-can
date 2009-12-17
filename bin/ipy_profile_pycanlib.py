e29787de417da1e03f4815b8ea6fae27c8cca6c5
bpowell <bpowell@localhost>
2009-12-17 14:57:10 +1300

    Fixed NameError in ipy_profile_pycanlib.py

--- bin/ipy_profile_pycanlib.py
+++ bin/ipy_profile_pycanlib.py
@@ -2,12 +2,10 @@
 File: ipy_profile_pycanlib.py
 
 This file is the ipython profile used to load pycanlib ipython extensions.
-It must be installed in the ~/_ipython/ folder on Windows or the 
-~/.ipython/ folder on Linux, where ~ is the user's home folder.
 """
 import IPython.ipapi
 import pycanlib.ipython
 
 ip_engine = IPython.ipapi.get()
 ip_engine.options.autocall = 2
-pycanlib.ipython.doIPythonImports(ipEngine)
+pycanlib.ipython.doIPythonImports(ip_engine)
