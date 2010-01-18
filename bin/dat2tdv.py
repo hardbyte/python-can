9a02a0ffab611eecb7798a846cf2d12cafa768d3
bpowell <bpowell@dynamiccontrols.com>
2010-01-19 10:46:02 +1300

    dat2tdv now opens dat files in binary mode (the pickle module documentation recommends this because the pickled data may have been written in binary mode)

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     _parser.add_option("-f", "--inputFile", dest="input_file",
       help="DAT file to decode")
     (_options, _args) = _parser.parse_args()
-    _in_file = open(_options.input_file, "r")
+    _in_file = open(_options.input_file, "rb")
     _log = cPickle.load(_in_file)
     _in_file.close()
     _out_file = open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w")
