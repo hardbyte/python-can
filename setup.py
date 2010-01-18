a7c6fcd79b825ded579b23a2afa0b5f9fca37546
bpowell <bpowell@localhost>
2010-01-19 11:47:19 +1300

    Moved GetVersionNumber from setup.py to CAN.py - setup.py now uses the current version number to generate a version.txt file for an installer package, while CAN.py attempts to use the current version number every time, and falls back to the number stored in version.txt if a Mercurial installation is unavailable

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 import os
 from setuptools import setup, find_packages
+import sys
 
+sys.path.append(".")
 
-def GetVersionNumber():
-    os.system("hg id > id.tmp")
-    tagFile = open("id.tmp", "r")
-    tagLine = tagFile.readline()
-    tag = tagLine.split(" ")[1].replace("\n", "")
-    if tag != "tip":
-        retVal = tag
-    else:
-        retVal = "dev_%s" % tagLine.split(" ")[0]
-    tagFile.close()
-    os.unlink("id.tmp")
-    return retVal
+from pycanlib import CAN
 
 if __name__ == "__main__":
-    _version = GetVersionNumber()
+    _version = CAN.get_version_number()
     _versionNumberFileName = "./pycanlib/version.txt"
     _versionFile = open(_versionNumberFileName, "w")
     _versionFile.write(_version)
