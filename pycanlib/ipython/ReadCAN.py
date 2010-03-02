1d5e4b4d2df83eef619bc8425ae0fe50fe0ed51a
bpowell <bpowell@localhost>
2010-03-02 13:21:38 +1300

    Unwrapped wrapped source line in ReadCAN Fixed crash in ReadCAN where the CAN bus was being closed before the user terminated the read operation Added WriteCAN ipython extension

--- pycanlib/ipython/ReadCAN.py
+++ pycanlib/ipython/ReadCAN.py
@@ -7,19 +7,18 @@ class ReadCAN(ipipe.Table):
 
     def __init__(self, channel, speed, tseg1, tseg2, sjw, no_samp):
         ipipe.Table.__init__(None)
-        self.bus = CAN.Bus(channel=channel, speed=speed, tseg1=tseg1, 
-          tseg2=tseg2, sjw=sjw, no_samp=no_samp)
+        self.bus = CAN.Bus(channel=channel, speed=speed, tseg1=tseg1, tseg2=tseg2, sjw=sjw, no_samp=no_samp)
         self.bus.enable_callback()
 
     def __iter__(self):
-        while True:
-            try:
+        try:
+            while True:
                 message = self.bus.read()
                 if message != None:
                     yield message
                 else:
                     time.sleep(0.001)
-            except KeyboardInterrupt:
-                break
-            finally:
-                self.bus.shutdown()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.bus.shutdown()
