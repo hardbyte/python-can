35c4d316f533695e322b1569f6396b363b64aba0
bpowell <bpowell@localhost>
2010-04-01 07:54:28 +1300

    Simplified getchanneldata.py

--- examples/getchanneldata.py
+++ examples/getchanneldata.py
@@ -1,29 +1,11 @@
 import ctypes
-import logging
 import sys
 
 from pycanlib import CAN, canlib
 
-def SetupLogging():
-    loggerObj = logging.getLogger("getchanneldata")
-    loggerObj.setLevel(logging.INFO)
-    _logStreamHandler = logging.StreamHandler()
-    _logFormatter = logging.Formatter("%(message)s")
-    _logStreamHandler.setFormatter(_logFormatter)
-    loggerObj.addHandler(_logStreamHandler)
-    return loggerObj
-
-def main():
-    _loggerObj = SetupLogging()
-    [_loggerObj.info(_line) for _line in CAN.get_host_machine_info().__str__().split("\n")]
-    buses = {}
-    canlib.canInitializeLibrary()
-    _numChannels = ctypes.c_int(0)
-    canlib.canGetNumberOfChannels(ctypes.byref(_numChannels))
-    for channel in xrange(0, _numChannels.value):
-        buses[channel] = CAN.Bus(channel, 1000000, 4, 3, 1, 3)
-        for line in buses[channel].channel_info.__str__().split("\n"):
-            _loggerObj.info(line)
-
 if __name__ == "__main__":
-    main()
+    [sys.stdout.write("%s\n" % _line) for _line in CAN.get_host_machine_info().__str__().split("\n")]
+    _num_channels = ctypes.c_int(0)
+    canlib.canGetNumberOfChannels(ctypes.byref(_num_channels))
+    for _channel in xrange(0, _num_channels.value):
+        [sys.stdout.write("%s\n" % _line) for _line in CAN.Bus(_channel, 1000000, 4, 3, 1, 3).channel_info.__str__().split("\n")]
