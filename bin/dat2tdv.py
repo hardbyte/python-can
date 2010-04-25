c3eb6c8fdb48160f11895178d25a02d96ecf6113
bpowell <bpowell@dynamiccontrols.com>
2010-04-26 09:13:06 +1200

    Changed file operations in dat2tdv.py to use "with" statement

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
     _parser = OptionParser()
     _parser.add_option("-f", "--inputFile", dest="input_file", help="DAT file to decode")
     (_options, _args) = _parser.parse_args()
-    _in_file = open(_options.input_file, "rb")
-    _log = cPickle.load(_in_file)
-    _in_file.close()
-    _out_file = open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w")
-    _out_file.write("%s" % _log)
-    _out_file.close()
+    with _in_file as open(_options.input_file, "rb"):
+        _log = cPickle.load(_in_file)
+    with _out_file as open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w")
+        _out_file.write("%s" % _log)
