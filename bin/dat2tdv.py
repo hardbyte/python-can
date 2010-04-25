265968d4f2408e30f64a6c082fd70fe0d5262b9d
bpowell <bpowell@localhost>
2010-04-26 09:53:48 +1200

    Fixed "with" statements in dat2tdv.py

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     _parser = OptionParser()
     _parser.add_option("-f", "--inputFile", dest="input_file", help="DAT file to decode")
     (_options, _args) = _parser.parse_args()
-    with _in_file as open(_options.input_file, "rb"):
+    with open(_options.input_file, "rb") as _in_file:
         _log = cPickle.load(_in_file)
-    with _out_file as open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w")
+    with open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w") as _out_file:
         _out_file.write("%s" % _log)
