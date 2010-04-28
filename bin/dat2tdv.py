49067a6d1008247e86a3d914804274afd82b3d13
bpowell <bpowell@localhost>
2010-04-28 14:49:43 +1200

    Changed flag used to pass input file to dat2tdv.py from -f to -i (-f will be used for the 'filter messages' option when it is implemented) Removed out of date comment from CAN.py

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -4,7 +4,7 @@ from optparse import OptionParser
 
 if __name__ == "__main__":
     _parser = OptionParser()
-    _parser.add_option("-f", "--inputFile", dest="input_file", help="DAT file to decode")
+    _parser.add_option("-i", "--inputFile", dest="input_file", help="DAT file to decode")
     (_options, _args) = _parser.parse_args()
     with open(_options.input_file, "rb") as _in_file:
         _log = cPickle.load(_in_file)
