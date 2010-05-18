ffc912f90e7113f4856f9d55f5d4a7d7f3c92ffb
bpowell <bpowell@dynamiccontrols.com>
2010-05-19 09:44:44 +1200

    Simplified LogCAN by abstracting parts of its functionality into the CAN.Log class

--- pycanlib/ipython/LogCAN.py
+++ pycanlib/ipython/LogCAN.py
@@ -9,14 +9,7 @@ class LogCAN(ipipe.Display):
 
     def __init__(self, input=None):
         ipipe.Display.__init__(self, input)
-        self.logStartTime = datetime.datetime.now()
-        _timestamp = self.logStartTime.strftime("%Y%m%d_%H%M%S")
-        _userdir = os.path.expanduser("~")
-        _path = os.path.join(_userdir, "LogCAN")
-        _datfilename = "LogCAN_%s.dat" % _timestamp
-        if not os.path.exists(_path):
-            os.makedirs(_path)
-        self.datfilename = _datfilename
+        self.__start_time = datetime.datetime.now()
         self.msglist = []
 
     def display(self):
@@ -27,15 +20,10 @@ class LogCAN(ipipe.Display):
                 self.msglist.append(item)
         except KeyboardInterrupt:
             pass
-        _log_info = CAN.LogInfo(log_start_time=self.logStartTime,
-                                log_end_time=datetime.datetime.now(),
-                                original_file_name=self.datfilename,
-                                tester_name=os.getenv("USERNAME"))
-        _machine_info = CAN.get_host_machine_info()
-        _message_lists = [CAN.MessageList(messages=self.msglist)]
-        _log_obj = CAN.Log(log_info=_log_info,
-                           channel_info=None,
-                           machine_info=_machine_info,
-                           message_lists=_message_lists)
-        with open(os.path.join(_path, self.datfilename), "w") as _datfile:
-            cPickle.dump(_log_obj, _datfile)
+        CAN.Log(log_info=CAN.LogInfo(log_start_time=self.__start_time,
+                                     log_end_time=datetime.datetime.now(),
+                                     original_file_name=("LogCAN_%s.dat" % self.__start_time.strftime("%Y%m%d_%H%M%S")),
+                                     tester_name=os.getenv("USERNAME")),
+                channel_info=None,
+                machine_info=CAN.get_host_machine_info(),
+                message_lists=[CAN.MessageList(messages=self.msglist)]).write_to_file(path=os.path.expanduser(os.path.join("~", "LogCAN")))
