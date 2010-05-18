19d556777f90e0f4dad76ac9fbb1b0f6cea4faa4
bpowell <bpowell@localhost>
2010-05-18 15:08:53 +1200

    Moved buffered CAN bus reader functionality from pycanlib.ipython.ReadCAN to pycanlib.CAN pycanlib.ipython.ReadCAN and can_logger.py now use pycanlib.CAN.BufferedReader to read from the bus

--- pycanlib/ipython/ReadCAN.py
+++ pycanlib/ipython/ReadCAN.py
@@ -3,28 +3,12 @@ import Queue
 
 from pycanlib import CAN
 
-class CANReader(CAN.Listener):
-    def __init__(self):
-        self.__msg_queue = Queue.Queue(0)
-
-    def on_message_received(self, msg):
-        self.__msg_queue.put_nowait(msg)
-
-    def on_status_change(self, timestamp, old_status, new_status):
-        pass
-
-    def get_message(self):
-        try:
-            return self.__msg_queue.get(timeout=1)
-        except Queue.Empty:
-            return None
-
 class ReadCAN(ipipe.Table):
 
     def __init__(self, channel, speed, tseg1, tseg2, sjw, no_samp):
         ipipe.Table.__init__(None)
         self.__bus = CAN.Bus(channel=channel, speed=speed, tseg1=tseg1, tseg2=tseg2, sjw=sjw, no_samp=no_samp)
-        self.__reader = CANReader()
+        self.__reader = CAN.BufferedReader()
         self.__bus.add_listener(self.__reader)
         self.__bus.enable_callback()
 
