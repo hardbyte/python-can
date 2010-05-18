d98cb8bafbfe303b333aa15bb5ef8bc670efe53e
bpowell <bpowell@localhost>
2010-05-18 15:41:31 +1200

    dat2tdv script now uses pycanlib.CAN.Log.write_to_file

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -8,5 +8,4 @@ if __name__ == "__main__":
     (_options, _args) = _parser.parse_args()
     with open(_options.input_file, "rb") as _in_file:
         _log = cPickle.load(_in_file)
-    with open(("%s.log" % os.path.splitext(_options.input_file)[0]), "w") as _out_file:
-        _out_file.write("%s" % _log)
+    _log.write_to_file(format=CAN.LOG_FORMAT_TDV, name=("%s.log" % os.path.splitext(_options.input_file)[0]), path=os.path.dirname(_options.input_file))
