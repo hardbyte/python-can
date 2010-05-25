5a59a8891341dfaf7b8beed942164d21a68d5a6e
bpowell <bpowell@localhost>
2010-05-26 07:50:32 +1200

    dat2tdv no longer uses optparse, as it takes only one argument, and that argument is not optional

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -1,13 +1,14 @@
 import cPickle
 import os
-from optparse import OptionParser
+import sys
 
 from pycanlib import CAN
 
 if __name__ == "__main__":
-    _parser = OptionParser()
-    _parser.add_option("-i", "--inputFile", dest="input_file", help="DAT file to decode")
-    (_options, _args) = _parser.parse_args()
-    with open(_options.input_file, "rb") as _in_file:
-        _log = cPickle.load(_in_file)
-    _log.write_to_file(format=CAN.LOG_FORMAT_TDV, name=("%s.log" % os.path.splitext(_options.input_file)[0]), path=os.path.dirname(_options.input_file))
+    if len(sys.argv) > 1:
+        try:
+            with open(sys.argv[1], "rb") as _in_file:
+                _log = cPickle.load(_in_file)
+            _log.write_to_file(format=CAN.LOG_FORMAT_TDV, name=("%s.log" % os.path.splitext(_options.input_file)[0]), path=os.path.dirname(_options.input_file))
+        except IOError:
+            sys.stderr.write("ERROR: Input file %s not found\n" % sys.argv[1])
