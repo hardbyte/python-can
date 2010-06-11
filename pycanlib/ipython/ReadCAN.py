6df0574d34e46a1f07de9aa07faac81c89b020cf
bpowell <bpowell@dynamiccontrols.com>
2010-06-11 14:42:37 +1200

    Updated ipython library to support simpler thread-based CAN.py - as a result, the utilities which read and write log files are currently unavailable, but will be added again when the supporting functionality they require has been added back to pycanlib

--- pycanlib/ipython/ReadCAN.py
+++ pycanlib/ipython/ReadCAN.py
@@ -1,21 +1,18 @@
 import ipipe
 import Queue
 
-from pycanlib import CAN
+from pycanlib import CAN, canlib
 
 class ReadCAN(ipipe.Table):
 
-    def __init__(self, channel, speed, tseg1, tseg2, sjw, no_samp):
+    def __init__(self, channel, bitrate, tseg1, tseg2, sjw, no_samp, driver_mode=canlib.canDRIVER_NORMAL):
         ipipe.Table.__init__(None)
-        self.__bus = CAN.Bus(channel=channel, speed=speed, tseg1=tseg1, tseg2=tseg2, sjw=sjw, no_samp=no_samp)
+        self.__bus = CAN.Bus(channel=channel, bitrate=bitrate, tseg1=tseg1, tseg2=tseg2, sjw=sjw, no_samp=no_samp, driver_mode=driver_mode)
         self.__reader = CAN.BufferedReader()
         self.__bus.add_listener(self.__reader)
 
     def __iter__(self):
-        try:
-            while True:
-                _msg = self.__reader.get_message()
-                if _msg != None:
-                    yield _msg
-        except KeyboardInterrupt:
-            pass
+        while True:
+            _msg = self.__reader.get_message()
+            if _msg != None:
+                yield _msg
