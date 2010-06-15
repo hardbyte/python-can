0dfcb3e7808b609fd8a7984451a839385f541eea
bpowell <bpowell@dynamiccontrols.com>
2010-06-15 15:01:21 +1200

    Fixed ParameterTypeError in WriteLog on systems with no "user name" environment variable - if the "user name" environment variable is not found, this value defaults to "None"

--- pycanlib/ipython/WriteLog.py
+++ pycanlib/ipython/WriteLog.py
@@ -23,7 +23,7 @@ class WriteLog(ipipe.Display):
         CAN.Log(log_info=CAN.LogInfo(log_start_time=self.__start_time,
                                      log_end_time=datetime.datetime.now(),
                                      original_file_name=("can_message_log_%s.dat" % self.__start_time.strftime("%Y%m%d_%H%M%S")),
-                                     tester_name=os.getenv("USERNAME")),
+                                     tester_name=("%s" % os.getenv("USERNAME"))),
                 channel_info=None,
                 machine_info=CAN.get_host_machine_info(),
                 message_lists=[CAN.MessageList(messages=self.msglist)]).write_to_file(path=os.path.expanduser(os.path.join("~", "WriteLog")))
