b960f8f5e20d97a917421d85feb7f99f087a595f
Ben Powell <bpowell@dynamiccontrols.com>
2010-06-23 11:36:30 +1200

    getchanneldata.py now closes the bus handles it opens so that the Python interpreter doesn't crash when the script exits

--- examples/getchanneldata.py
+++ examples/getchanneldata.py
@@ -1,5 +1,6 @@
 import ctypes
 import sys
+import time
 
 from pycanlib import CAN, canlib
 
@@ -8,4 +9,8 @@ if __name__ == "__main__":
     _num_channels = ctypes.c_int(0)
     canlib.canGetNumberOfChannels(ctypes.byref(_num_channels))
     for _channel in xrange(0, _num_channels.value):
-        [sys.stdout.write("%s\n" % _line) for _line in CAN.Bus(_channel, 1000000, 4, 3, 1, 3).channel_info.__str__().split("\n")]
+        _bus = CAN.Bus(_channel, 1000000, 4, 3, 1, 3)
+        [sys.stdout.write("%s\n" % _line) for _line in _bus.channel_info.__str__().split("\n")]
+        _bus.shutdown()
+    time.sleep(1)
+
