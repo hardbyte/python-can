b6ae9b1fa4d1377be524d8efaa0e15e28a9624b6
bpowell <bpowell@localhost>
2010-07-01 10:34:37 +1200

    Added GNU GPL preamble to most files in the pycanlib package - the SRS, manual, contributors file and LaTeX stylesheet for the SRS and manual, have been left out for now as they are licensed differently. Removed DX bus parameter defaults from busload.py example Added newline at end of pycanlib/__init__.py

--- bin/dat2tdv.py
+++ bin/dat2tdv.py
@@ -1,3 +1,36 @@
+"""
+dat2tdv.py: part of pycanlib, converts pickled (*.dat) log files generated
+by can_logger.py and the WriteLog ipython utility to human-readable TDV
+format.
+
+Copyright (C) 2010 Dynamic Controls
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact details
+---------------
+
+Postal address:
+    Dynamic Controls
+    17 Print Place
+    Addington
+    Christchurch 8024
+    New Zealand
+
+E-mail: bpowell AT dynamiccontrols DOT com
+"""
+
 import cPickle
 import os
 import sys
--- pycanlib/ipython/__init__.py
+++ pycanlib/ipython/__init__.py
@@ -1,3 +1,33 @@
+"""
+ipython/__init__.py
+
+Copyright (C) 2010 Dynamic Controls
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact details
+---------------
+
+Postal address:
+    Dynamic Controls
+    17 Print Place
+    Addington
+    Christchurch 8024
+    New Zealand
+
+E-mail: bpowell AT dynamiccontrols DOT com
+"""
 def doIPythonImports(ip_engine):
     ip_engine.ex("print 'Importing ipipe library...'")
     ip_engine.ex("from ipipe import *")
