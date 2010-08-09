5199d34ec97ecac6f3ff12865a4221155b43b479
bpowell <bpowell@localhost>
2010-08-09 13:10:49 +1200

    CAN.Bus.listeners is now accessible as a property, and the add_listener and remove_listener methods have been removed

--- pycanlib/ipython/ReadCAN.py
+++ pycanlib/ipython/ReadCAN.py
@@ -41,7 +41,7 @@ class ReadCAN(ipipe.Table):
         ipipe.Table.__init__(None)
         self.__bus = CAN.Bus(channel=channel, bitrate=bitrate, tseg1=tseg1, tseg2=tseg2, sjw=sjw, no_samp=no_samp, driver_mode=driver_mode)
         self.__reader = CAN.BufferedReader()
-        self.__bus.add_listener(self.__reader)
+        self.__bus.listeners.append(self.__reader)
 
     def __iter__(self):
         try:
