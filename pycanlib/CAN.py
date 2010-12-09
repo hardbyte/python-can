73e76c72f8ea38dc297f379449a38b789b8a1f40
Mark Catley <mcatley@dynamiccontrols.com>
2010-12-09 14:49:24 +1300

    Added TimestampMessage object to CAN.py, which is now returned instead of None when no messages are available

--- pycanlib/CAN.py
+++ pycanlib/CAN.py
@@ -341,6 +341,42 @@ class Message(object):
             _field_strings.append(("\n" + (" " * (_current_length + 4))).join(self.info_strings))
         return "    ".join(_field_strings)
 
+class TimestampMessage(object):
+    def __init__(self, timestamp=0.0, info_strings=None):
+        self.timestamp = timestamp
+        if info_strings is not None:
+            self.info_strings = info_strings
+        else:
+            self.info_strings = []
+
+    @property
+    def timestamp(self):
+        return self.__timestamp
+
+    @timestamp.setter
+    def timestamp(self, value):
+        InputValidation.verify_parameter_min_value("CAN.TimestampMessage.timestamp.setter", "timestamp", value, 0)
+        self.__timestamp = float(value)
+
+    @property
+    def info_strings(self):
+        return self.__info_strings
+
+    @info_strings.setter
+    def info_strings(self, value):
+        self.__info_strings = value
+
+    def check_equality(self, other, fields):
+        return False
+
+    def __str__(self):
+        if len(self.info_strings) > 0:
+            retval = "%15.6f    " % self.timestamp
+            retval += ("\n"+" "*len("%15.6f    " % self.timestamp)).join(self.info_strings)
+        else:
+            retval = ""
+        return retval
+
 class MessageList(object):
 
     def __init__(self, messages=[], filter_criteria="True", name="default"):
@@ -874,7 +910,7 @@ class Bus(object):
             _rx_msg.flags = int(_flags.value) & canstat.canMSG_MASK
             return _rx_msg
         else:
-            return None
+            return TimestampMessage(timestamp=self.bus_time)
 
     def __write_process(self):
         while self.__threads_running:
