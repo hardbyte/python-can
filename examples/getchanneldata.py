2c4c462c02226dd9a8e84df70f386b80dd707949
Brian Thorne <BThorne@dynamiccontrols.com>
2012-02-02 12:11:53 +1300

    Few docs changes, removed lots of the gpl3 headers. Remove references to external C module.

--- examples/getchanneldata.py
+++ examples/getchanneldata.py
@@ -1,34 +1,7 @@
 """
-getchanneldata.py: an example script illustrating access to
+An example script illustrating access to
 channel data available for a CAN bus channels connected to a
 computer.
-
-Copyright (C) 2010 Dynamic Controls
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Contact details
----------------
-
-Postal address:
-    Dynamic Controls
-    17 Print Place
-    Addington
-    Christchurch 8024
-    New Zealand
-
-E-mail: bpowell AT dynamiccontrols DOT com
 """
 import ctypes
 import sys
@@ -37,13 +10,14 @@ import time
 from pycanlib import CAN, canlib
 
 if __name__ == "__main__":
-    print CAN.get_host_machine_info()
-    _num_channels = ctypes.c_int(0)
-    canlib.canGetNumberOfChannels(ctypes.byref(_num_channels))
-    for _channel in xrange(0, _num_channels.value):
-        _bus = CAN.Bus(_channel, 1000000, 4, 3, 1, 3)
-        print _bus.channel_info
-        _bus.shutdown()
+    print(CAN.get_host_machine_info())
+
+    num_channels = ctypes.c_int(0)
+    canlib.canGetNumberOfChannels(ctypes.byref(num_channels))
+    for channel in range(0, num_channels.value):
+        bus = CAN.Bus(channel, 1000000, 4, 3, 1, 3)
+        print(bus.channel_info)
+        bus.shutdown()
 
 
 time.sleep(1)
--- setup.py
+++ setup.py
@@ -1,35 +1,9 @@
 """
-setup.py: contains the functionality used to create installer packages
+Contains the functionality used to create installer packages
 for pycanlib, as well as installing it from source. It requires the
 setuptools package to be installed.
-
-Copyright (C) 2010 Dynamic Controls
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Contact details
----------------
-
-Postal address:
-    Dynamic Controls
-    17 Print Place
-    Addington
-    Christchurch 8024
-    New Zealand
-
-E-mail: bpowell AT dynamiccontrols DOT com
 """
+
 import os
 from setuptools import setup, find_packages, Extension
 
@@ -47,8 +21,7 @@ setup(
     name="pycanlib",
     version=_version,
     packages=find_packages(exclude=["test"]),
-    py_modules = ['ipy_profile_pycanlib'],
-    ext_modules = [Extension('pythonSocketCan', ['canSocket.c'])],
+    py_modules = ['pycanlib'],
     author="Ben Powell",
     author_email="bpowell AT dynamiccontrols DOT com",
     description="Python wrapper for Kvaser's CANLIB SDK",
