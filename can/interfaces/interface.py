1f54dfdddcfaff38c33ca403771c18b180558f82
Bruno Pennati < bruno@tracciabi.li >
2015-06-24 10:34:29 +0200

    [interface.py] refs #32 Improve interface.py checks with bustype='socketcan'
    When creating a Bus with bustype='socketcan' the operating system and
    python version are checked to make sure wether we can really use
    socketcan_native or socketcan_ctypes.
    Also, *.sw[p|o] vim swap files added to .hgignore

--- can/interfaces/interface.py
+++ can/interfaces/interface.py
@@ -18,11 +18,36 @@ class Bus(object):
             elif kwargs['bustype'] == 'socketcan_native':
                 can.rc['interface'] = 'socketcan_native'
             elif kwargs['bustype'] == 'socketcan':
-                try:
-                    import fcntl
-                    can.rc['interface'] = 'socketcan_native'
-                except ImportError:
-                    can.rc['interface'] = 'socketcan_ctypes'
+                # Check OS: SocketCAN is available only under Linux
+                import sys
+                if sys.platform.startswith('linux'):
+                    # Check release: SocketCAN was added to Linux 2.6.25
+                    import platform
+                    import re
+                    rel_string = platform.release()
+                    m = re.match('\d+\.\d+\.\d', rel_string)
+                    if m is None:
+                        msg = 'Bad linux release {}'.format(rel_string)
+                        raise Exception(msg)
+                    rel_num = [int(i) for i in rel_string[:m.end()].split('.')]
+                    print('rel_num {}'.format(rel_num))
+                    if (rel_num >= [2, 6, 25]):
+                        # Check Python version: SocketCAN was added in 3.3
+                        py_ver = [sys.version_info[0], sys.version_info[1]]
+                        if (py_ver >= [3, 3]):
+                            print('python >= 3.3')
+                            can.rc['interface'] = 'socketcan_native'
+                        else:
+                            print('python < 3.3')
+                            can.rc['interface'] = 'socketcan_ctypes'
+                    else:
+                        msg = 'SocketCAN not available under Linux {}'.format(
+                                rel_string)
+                        raise Exception(msg)
+                else:
+                    msg = 'SocketCAN not available under {}'.format(
+                        sys.platform)
+                    raise Exception(msg)
             elif kwargs['bustype'] == 'serial':
                 can.rc['interface'] = 'serial'
             elif kwargs['bustype'] == 'pcan':
