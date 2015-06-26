0f964e0e626d4011276e9e267bc4d5f681f15d59
jvmotivps <jvmotivps@localhost>
2015-06-26 14:09:47 -0700

    Adding interface notes and fixed imports

--- can/interfaces/usb2canInterface.py
+++ can/interfaces/usb2canInterface.py
@@ -1,11 +1,59 @@
-from usb2can import *
+#this interface is for windows only, otherwise use socketCAN
+
 import logging
 
 logger = logging.getLogger(__name__)
 
-from can.interfaces.PCANBasic import *
+from can.interfaces.usb2can import *
 from can.bus import BusABC
 from can.message import Message
+
+
+#code to handle finding devices
+
+import win32com.client
+def WMIDateStringToDate(dtmDate):
+    strDateTime = ""
+    if (dtmDate[4] == 0):
+        strDateTime = dtmDate[5] + '/'
+    else:
+        strDateTime = dtmDate[4] + dtmDate[5] + '/'
+    if (dtmDate[6] == 0):
+        strDateTime = strDateTime + dtmDate[7] + '/'
+    else:
+        strDateTime = strDateTime + dtmDate[6] + dtmDate[7] + '/'
+        strDateTime = strDateTime + dtmDate[0] + dtmDate[1] + dtmDate[2] + dtmDate[3] + " " + dtmDate[8] + dtmDate[9] + ":" + dtmDate[10] + dtmDate[11] +':' + dtmDate[12] + dtmDate[13]
+    return strDateTime
+
+strComputer = "."
+objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+objSWbemServices = objWMIService.ConnectServer(strComputer,"root\cimv2")
+colItems = objSWbemServices.ExecQuery("SELECT * FROM Win32_USBControllerDevice")
+for objItem in colItems:
+    if objItem.AccessState != None:
+        print "AccessState:" + ` objItem.AccessState`
+    if objItem.Antecedent != None:
+        print "Antecedent:" + ` objItem.Antecedent`
+    if objItem.Dependent != None:
+        print "Dependent:" + ` objItem.Dependent`
+    if objItem.NegotiatedDataWidth != None:
+        print "NegotiatedDataWidth:" + ` objItem.NegotiatedDataWidth`
+    if objItem.NegotiatedSpeed != None:
+        print "NegotiatedSpeed:" + ` objItem.NegotiatedSpeed`
+    if objItem.NumberOfHardResets != None:
+        print "NumberOfHardResets:" + ` objItem.NumberOfHardResets`
+    if objItem.NumberOfSoftResets != None:
+        print "NumberOfSoftResets:" + ` objItem.NumberOfSoftResets`
+
+
+
+
+
+
+
+
+
+#end of device finding code
 
 boottimeEpoch = 0
 try:
