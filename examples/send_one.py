ea6c7387992e6cca8822497bd46ac4b368f54550
Bruno Pennati <bruno@tracciabi.li>
2015-06-26 11:27:21 +0200

    [send_one.py] Warn the user when bus.send fails

--- examples/send_one.py
+++ examples/send_one.py
@@ -8,8 +8,10 @@ def main():
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
                       extended_id=False)
-    bus.send(msg)
-    print("Message sent")
+    if bus.send(msg) < 0:
+        print("Message NOT sent")
+    else:
+        print("Message sent")
 
 if __name__ == "__main__":
     main()
