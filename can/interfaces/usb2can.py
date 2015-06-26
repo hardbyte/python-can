767734c528ce7847648f2cd3c144925e6cf362f3
jvmotivps <jvmotivps@localhost>
2015-06-26 13:16:24 -0700

    Adding python wrapper for CANAL interface for usb2can device

---
+++ can/interfaces/usb2can.py
@@ -0,0 +1,198 @@
+from ctypes import *
+from struct import *
+#import collections
+
+#type definitions
+
+flags = c_ulong
+pConfigureStr = c_char_p
+handle = c_long
+timeout = c_ulong
+filter = c_ulong
+
+#attempted to use namedtuples, cannot name types of variables easily
+#dataRaw = c_char
+#dataRaw = c_char
+
+#msg = collections.namedtuple('CanalMsg', ['flags', 'obid', 'id', 'sizeDaa', 'data', 'timestamp'])
+
+#msg(flags = c_ulong, obid = c_ulong, id = c_ulong, sizeData = c_char, data = dataRaw, timestamp = c_ulong)
+
+
+typedef struct structCanalStatistics {
+    unsigned long cntReceiveFrames;             // # of receive frames
+    unsigned long cntTransmitFrames;            // # of transmitted frames
+    unsigned long cntReceiveData;               // # of received data bytes
+    unsigned long cntTransmitData;              // # of transmitted data bytes	
+    unsigned long cntOverruns;                  // # of overruns
+    unsigned long cntBusWarnings;               // # of bys warnings
+    unsigned long cntBusOff;                    // # of bus off's
+} canalStatistics;
+
+class CanalStatistics(Structure):
+
+	_fields_ = [('ReceiveFrams', c_ulong,
+		('TransmistFrams', c_ulong),
+		('ReceiveData', c_ulong),
+		('TransmitData', c_ulong),
+		('Overruns', c_ulong),
+		('BusWarnings', c_ulong),
+		('BusOff', c_ulong)]
+
+stat = CanalStatistics
+
+class CanalStatus(Structure):
+	_fields_ = [('channel_status', c_ulong),
+		('lasterrorcode', c_ulong),
+		('lasterrorsubcode', c_ulong),
+		('lasterrorstr', c_byte * 80)]
+	
+
+#data type for the CAN Message
+class CanalMsg(Structure):
+	
+	
+	_fields_ = [('flag', c_ulong), 
+		('obid', c_ulong), 
+		('id', c_ulong), 
+		('sizeData', c_ubyte), 
+		('data', c_ubyte * 8), 
+		('timestamp', c_ulong)]
+	
+msg = CanalMsg		
+	
+class usb2can:
+	
+	def __init__(self):
+		self.__m_dllBasic = windll.LoadLibrary("usb2can.dll")
+		
+		if self.__m_dllBasic == None:
+			print "DLL cannot be loaded"
+	
+
+
+
+	def CanalOpen(self, pConfigureStr, flags):
+		try:
+			res = self.__m_dllBasic.CanalOpen(pConfigureStr, flags)
+			return res
+		except:
+			print "Could not open"
+			raise
+	def CanalClose(self, handle):
+		try:
+			res = self.__m_dllBasic.CanalClose(handle)
+			return res
+		except:
+			print "could not close port"
+			raise
+	def CanalSend(self, handle, msg):
+		try:	
+			res = self.__m_dllBasic.CanalSend(handle, msg)
+			return res
+		except:
+			print "sending error"
+			raise
+	def CanalReceive(self, handle, msg):
+		try:	
+			res = self.__m_dllBasic.CanalReceive(handle, msg)
+			return res
+		except:
+			print "Receive Error"
+			raise		
+		
+	def CanalBlockingSend(self, handle, msg, timeout):
+		try:	
+			res = self.__m_dllBasic.CanalBlockingSend(handle, msg, timeout)
+			return res
+		except:
+			print "sending error"
+			raise		
+	
+	def CanalBlockingReceive(self, handle, msg, timeout):
+		try:	
+			res = self.__m_dllBasic.CanalBlockingReceive(handle, msg, timeout)
+			return res
+		except:
+			print "receive Error"
+			raise		
+		
+	def CanalGetStatus(self, handle, CanalStatus):
+		try:
+			res = self.__m_dllBasic.CanalGetStatus(handle, CanalStatus)
+			return res
+		except:
+			print "failed to get status"
+			raise
+	
+	
+	
+	
+	def CanalGetStatistics(self, handle, CanalStatistics):
+		try:	
+			res = self.__m_dllBasic.CanalGetStatistics(handle, CanalStatistics)
+			return res
+		except:
+			print "sending error"
+			raise
+	
+	
+	
+	
+	
+	def CanalGetVersion(self):
+		try:	
+			res = self.__m_dllBasic.CanalGetVersion()
+			return res
+		except:
+			print "sending error"
+			raise
+	
+	
+	
+	
+	
+	def CanalGetDllVersion(self):
+		try:	
+			res = self.__m_dllBasic.CanalGetDllVersion()
+			return res
+		except:
+			print "sending error"
+			raise
+	
+	
+	
+	
+	
+	def CanalGetVendorString(self):
+		try:	
+			res = self.__m_dllBasic.CanalGetVendorString()
+			return res
+		except:
+			print "sending error"
+			raise	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+#CanalBlockingSend
+#CanalReceive
+#CanalBlockingReceive
+#CanalDataAvailable
+#canalGetStatus
+#CanalGetStatistics
+#CanalSetFilter
+#CanalSetMask
+#CanalSetBaudrate
+#CanalGetVersion
+#CanalGetDLLVerion
+#CanalGetVendorString
+
+
+#CANALMSG
