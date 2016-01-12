75e869a4e4f6e3f9c3d8b9beefc77d7b5291b443
Brian Thorne <hardbyte@users.noreply.github.com>
2016-01-12 18:18:51 +1100

    Fix broadcast manager api and example

--- examples/cyclic.py
+++ examples/cyclic.py
@@ -4,23 +4,24 @@ This example exercises the periodic sending capabilities.
 
 Expects a vcan0 interface:
 
-        python3 -m examples.cyclic
+    python3 -m examples.cyclic
 
 """
 
 import logging
 import time
 
-logging.basicConfig(level=logging.INFO)
 import can
-can.rc['interface'] = 'socketcan_ctypes'
+logging.basicConfig(level=logging.INFO)
 
-from can.interfaces.interface import Message, MultiRateCyclicSendTask
+
+channel = 'vcan0'
+
 
 
 def test_simple_periodic_send():
     print("Trying to send a message...")
-    msg = Message(arbitration_id=0x0cf02200, data=[0, 1, 3, 1, 4, 1])
+    msg = can.Message(arbitration_id=0x0cf02200, data=[0, 1, 3, 1, 4, 1])
     task = can.send_periodic('vcan0', msg, 0.020)
     time.sleep(2)
 
@@ -41,9 +42,9 @@ def test_simple_periodic_send():
 
 def test_dual_rate_periodic_send():
     """Send a message 10 times at 1ms intervals, then continue to send every 500ms"""
-    msg = Message(arbitration_id=0x0c112200, data=[0, 1, 2, 3, 4, 5])
+    msg = can.Message(arbitration_id=0x0c112200, data=[0, 1, 2, 3, 4, 5])
     print("Creating cyclic task to send message 10 times at 1ms, then every 500ms")
-    task = MultiRateCyclicSendTask('vcan0', msg, 10, 0.001, 0.50)
+    task = can.interface.MultiRateCyclicSendTask('vcan0', msg, 10, 0.001, 0.50)
     time.sleep(2)
 
     print("Changing data[0] = 0x42")
@@ -64,5 +65,15 @@ def test_dual_rate_periodic_send():
 
 if __name__ == "__main__":
 
-    #test_simple_periodic_send()
-    test_dual_rate_periodic_send()
+    for interface in {'socketcan_ctypes', 'socketcan_native'}:
+        print("Carrying out cyclic tests with {} interface".format(interface))
+        can.rc['interface'] = interface
+        test_simple_periodic_send()
+
+
+    for interface in {'socketcan_ctypes', }:
+        print("Carrying out cyclic tests with {} interface".format(interface))
+        can.rc['interface'] = interface
+        test_dual_rate_periodic_send()
+
+    print("socketcan_native doesn't yet support MultiRate Cyclic Tasks")
