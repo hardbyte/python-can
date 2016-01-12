edeec67df2926781497f245514b11f457d0b4d1d
Brian Thorne <hardbyte@gmail.com>
2016-01-12 18:16:35 +1100

    Update send_one example to correctly deal with failed messages.

--- examples/send_one.py
+++ examples/send_one.py
@@ -2,16 +2,16 @@ from __future__ import print_function
 
 import can
 
-
-def main():
+def send_one():
     bus = can.interface.Bus()
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
                       extended_id=False)
-    if bus.send(msg) < 0:
+    try:
+        bus.send(msg)
+        print("Message sent on {}".format(bus.channel_info))
+    except can.CanError:
         print("Message NOT sent")
-    else:
-        print("Message sent")
 
 if __name__ == "__main__":
-    main()
+    send_one()
