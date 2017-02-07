850f67e298eb3a26d65992522d9351186bb41db3
Brian Thorne <brian@thorne.link>
2017-02-07 23:14:45 +1100

    Further splitting of the CyclicTask types into more composable units

--- can/broadcastmanager.py
+++ can/broadcastmanager.py
@@ -12,7 +12,21 @@ log = logging.getLogger('can.bcm')
 log.debug("Loading base broadcast manager functionality")
 
 
-class CyclicSendTaskABC(object):
+class CyclicTask(object):
+    """
+    Abstract Base for all Cyclic Tasks
+    """
+
+    @abc.abstractmethod
+    def stop(self):
+        """Cancel this periodic task.
+        """
+
+
+class CyclicSendTaskABC(CyclicTask):
+    """
+    Message send task with defined period
+    """
 
     def __init__(self, message, period):
         """
@@ -21,11 +35,25 @@ class CyclicSendTaskABC(object):
         """
         self.can_id = message.arbitration_id
         self.period = period
+        super(CyclicSendTaskABC, self).__init__()
 
-    @abc.abstractmethod
-    def stop(self):
-        """Cancel this periodic task.
+
+class LimitedDurationCyclicSendTaskABC(CyclicSendTaskABC):
+
+    def __init__(self, message, period, duration):
+        """Message send task with a defined duration and period.
+
+        :param message: The :class:`can.Message` to be sent periodically.
+        :param float period: The rate in seconds at which to send the message.
+        :param float duration:
+            The duration to keep sending this message at given rate.
         """
+        super(LimitedDurationCyclicSendTaskABC, self).__init__(message, period)
+        self.duration = duration
+
+
+class RestartableCyclicTaskABC(CyclicSendTaskABC):
+    """Adds support for restarting a stopped cyclic task"""
 
     @abc.abstractmethod
     def start(self):
