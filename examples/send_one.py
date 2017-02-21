aa10354386ed80dd7c94cba99d7655965defe71b
Christian Sandberg <christiansandberg@me.com>
2017-02-21 09:46:17 +0100

    Fix example which should use extended ID

--- examples/send_one.py
+++ examples/send_one.py
@@ -6,7 +6,7 @@ def send_one():
     bus = can.interface.Bus()
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
-                      extended_id=False)
+                      extended_id=True)
     try:
         bus.send(msg)
         print("Message sent on {}".format(bus.channel_info))
