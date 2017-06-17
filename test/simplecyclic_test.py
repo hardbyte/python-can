4ce6cf652eb3d0c810405012d2dc59de90d8bee2
Christian Sandberg <christiansandberg@me.com>
2017-06-17 13:33:52 +0200

    Fix cyclic test case

--- test/simplecyclic_test.py
+++ test/simplecyclic_test.py
@@ -11,15 +11,16 @@ class SimpleCyclicSendTaskTest(unittest.TestCase):
     def test_cycle_time(self):
         msg = can.Message(extended_id=False, arbitration_id=0x100, data=[0,1,2,3,4,5,6,7])
         bus = can.interface.Bus(bustype='virtual')
+        bus2 = can.interface.Bus(bustype='virtual')
         task = bus.send_periodic(msg, 0.01, 1)
         self.assertIsInstance(task, can.broadcastmanager.CyclicSendTaskABC)
         sleep(1.5)
-        size = bus.queue.qsize()
+        size = bus2.queue.qsize()
         print(size)
         # About 100 messages should have been transmitted. Some overhead will
         # make it less though
         self.assertTrue(90 < size < 110)
-        last_msg = bus.recv()
+        last_msg = bus2.recv()
         self.assertEqual(last_msg, msg)
 
 
