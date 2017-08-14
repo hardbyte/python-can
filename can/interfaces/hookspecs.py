a129a350a82bc12fd0d9dde7ae0fc22adaad9042
Pierre-Luc Tessier Gagné <ptessie3@ford.com>
2017-08-14 12:30:12 -0400

    Using pluggy to allow plugable can interfaces

--- can/interfaces/hookspecs.py
+++ can/interfaces/hookspecs.py
@@ -6,8 +6,6 @@ hookspec = HookspecMarker("pythoncan")
 
 @hookspec
 def pythoncan_interface(interface):
-    """ return dict containing python-can interface info.
-    The following keys are defined:
-        "module" - the class implementing the interface API
-        "classname" - name for selection from command line
+    """ return tuple (module, classname) containing python-can interface info
+    for the interface if supported by this implementation
     """
