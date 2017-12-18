54f922d7d72e2cb11e84f3f3dafb4f9705b88856
Felix Divo <felix.divo@gmail.com>
2017-12-18 08:01:38 +0100

    restructured test cases

--- test/listener_test.py
+++ test/listener_test.py
@@ -66,51 +66,6 @@ class ListenerTest(BusTest):
         m = a_listener.get_message(0.2)
         self.assertIsNotNone(m)
 
-    def testSQLWriterWritesToSameFile(self):
-        f = tempfile.NamedTemporaryFile('w', delete=False)
-        f.close()
-
-        first_listener = can.SqliteWriter(f.name)
-        first_listener(generate_message(0x01))
-
-        sleep(first_listener.MAX_TIME_BETWEEN_WRITES)
-        first_listener.stop()
-
-        second_listener = can.SqliteWriter(f.name)
-        second_listener(generate_message(0x02))
-
-        sleep(second_listener.MAX_TIME_BETWEEN_WRITES)
-
-        second_listener.stop()
-
-        con = sqlite3.connect(f.name)
-
-        with con:
-            c = con.cursor()
-
-            c.execute("select COUNT() from messages")
-            self.assertEqual(2, c.fetchone()[0])
-
-            c.execute("select * from messages")
-            msg1 = c.fetchone()
-            msg2 = c.fetchone()
-
-        self.assertEqual(msg1[1], 0x01)
-        self.assertEqual(msg2[1], 0x02)
-
-
-class BLFTest(unittest.TestCase):
-
-    def test_reader(self):
-        logfile = os.path.join(os.path.dirname(__file__), "data", "logfile.blf")
-        messages = list(can.BLFReader(logfile))
-        self.assertEqual(len(messages), 1)
-        self.assertEqual(messages[0],
-                         can.Message(
-                             extended_id=False,
-                             arbitration_id=0x64,
-                             data=[0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]))
-
 
 if __name__ == '__main__':
     unittest.main()
