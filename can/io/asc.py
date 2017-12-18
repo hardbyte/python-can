306e7b63df7343ebb6001f6e2b90fd961dd118b5
Felix Divo <felix.divo@gmail.com>
2017-12-18 08:01:19 +0100

    fixed a bug that occured in ASCReader/ASCWriter when empty messages were sent

--- can/io/asc.py
+++ can/io/asc.py
@@ -37,7 +37,13 @@ class ASCReader(object):
             temp = line.strip()
             if not temp or not temp[0].isdigit():
                 continue
-            (timestamp, channel, dummy) = temp.split(None, 2) # , frameType, dlc, frameData
+
+            try:
+                (timestamp, channel, dummy) = temp.split(None, 2) # , frameType, dlc, frameData
+            except ValueError:
+                # we parsed an empty comment
+                continue
+
             timestamp = float(timestamp)
 
             if dummy.strip()[0:10] == 'ErrorFrame':
@@ -45,7 +51,7 @@ class ASCReader(object):
                 yield msg
 
             elif not channel.isdigit() or dummy.strip()[0:10] == 'Statistic:':
-                pass
+                pass # continue
 
             elif dummy[-1:].lower() == 'r':
                 (can_id_str, _) = dummy.split(None, 1)
@@ -109,6 +115,11 @@ class ASCWriter(Listener):
 
     def log_event(self, message, timestamp=None):
         """Add an arbitrary message to the log file."""
+
+        if not message: # if empty or None
+            logger.debug("ASCWriter: ignoring empty message")
+            return
+
         timestamp = (timestamp or time.time())
         if timestamp >= self.started:
             timestamp -= self.started
