1538449a28a8d75f89805d12df20de9cd226f05d
Pierre-Luc Tessier Gagné <ptessie3@ford.com>
2018-01-17 10:54:22 -0500

    Adding string representation to CAN Bus Abstract Base Class
    Adding __str__ to CAN Bus Abstract Base Class for a nicely printable
    string representation of an the Bus using channel_info.

--- can/bus.py
+++ can/bus.py
@@ -47,6 +47,9 @@ class BusABC(object):
         """
         pass
 
+    def __str__(self):
+        return self.channel_info
+
     @abc.abstractmethod
     def recv(self, timeout=None):
         """Block waiting for a message from the Bus.
