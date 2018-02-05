196027c7e001132056317c07cd6576120706132e
Mathias Giacomuzzi <ecam@gmx.at>
2018-02-05 12:54:45 +0100

    fix decode error for pcan

--- examples/send_one.py
+++ examples/send_one.py
@@ -3,10 +3,7 @@ import can
 
 
 def send_one():
-    bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
-    #bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
-    #bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
-
+    bus = can.interface.Bus()
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
                       extended_id=True)
