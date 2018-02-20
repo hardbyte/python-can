fab6f23a8ea5dc325e560bc6e0ba0f98a90aacef
Felix Divo <felix.divo@gmail.com>
2018-02-20 18:37:53 +0100

    general cleanup of the examples

--- examples/send_one.py
+++ examples/send_one.py
@@ -1,20 +1,31 @@
-from __future__ import print_function
-import can
+#!/usr/bin/env python
+# coding: utf-8
 
+"""
+This example shows how sending a single message works.
+"""
+
+from __future__ import print_function
+
+import can
 
 def send_one():
     bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+
+    # Other buses work similar:
+    #bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
     #bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
     #bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
 
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
                       extended_id=True)
+
     try:
         bus.send(msg)
         print("Message sent on {}".format(bus.channel_info))
     except can.CanError:
         print("Message NOT sent")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     send_one()
