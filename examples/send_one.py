ea8e107f4ab241606717a6f0ebe172874b996fd7
Felix Divo <felix.divo@gmail.com>
2018-02-21 16:21:34 +0100

    better send_one example

--- examples/send_one.py
+++ examples/send_one.py
@@ -10,12 +10,17 @@ from __future__ import print_function
 import can
 
 def send_one():
-    bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
 
-    # Other buses work similar:
-    #bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
-    #bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
-    #bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
+    # this uses the default configuration (for example from the config file)
+    # see http://python-can.readthedocs.io/en/latest/configuration.html
+    bus = can.interface.Bus()
+
+    # Using specific buses works similar:
+    # bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
+    # bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+    # bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
+    # bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
+    # ...
 
     msg = can.Message(arbitration_id=0xc0ffee,
                       data=[0, 25, 0, 1, 3, 1, 4, 1],
