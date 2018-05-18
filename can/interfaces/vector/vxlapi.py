f588b1bccc04e5022e19fa2624b163e98f789bbf
AntonioCohimbra <38559979+AntonioCohimbra@users.noreply.github.com>
2018-05-18 13:25:36 +0200

    add initial version of CAN FD support for Vector

--- can/interfaces/vector/vxlapi.py
+++ can/interfaces/vector/vxlapi.py
@@ -29,12 +29,24 @@ XL_BUS_TYPE_CAN = 0x00000001
 XL_ERR_QUEUE_IS_EMPTY = 10
 
 XL_RECEIVE_MSG = 1
+XL_CAN_EV_TAG_RX_OK = 1024
+XL_CAN_EV_TAG_TX_OK = 1028
 XL_TRANSMIT_MSG = 10
+XL_CAN_EV_TAG_TX_MSG = 1088
 
 XL_CAN_EXT_MSG_ID = 0x80000000
 XL_CAN_MSG_FLAG_ERROR_FRAME = 0x01
 XL_CAN_MSG_FLAG_REMOTE_FRAME = 0x10
 XL_CAN_MSG_FLAG_TX_COMPLETED = 0x40
+
+XL_CAN_TXMSG_FLAG_EDL = 0x0001
+XL_CAN_TXMSG_FLAG_BRS = 0x0002
+XL_CAN_TXMSG_FLAG_RTR = 0x0010
+XL_CAN_RXMSG_FLAG_EDL = 0x0001
+XL_CAN_RXMSG_FLAG_BRS = 0x0002
+XL_CAN_RXMSG_FLAG_ESI = 0x0004
+XL_CAN_RXMSG_FLAG_RTR = 0x0010
+XL_CAN_RXMSG_FLAG_EF = 0x0200 
 
 XL_CAN_STD = 1
 XL_CAN_EXT = 2
@@ -45,8 +57,13 @@ XLhandle = ctypes.c_void_p
 
 MAX_MSG_LEN = 8
 
+XL_CAN_MAX_DATA_LEN = 64
+
 # current version
 XL_INTERFACE_VERSION = 3
+XL_INTERFACE_VERSION_V4 = 4
+
+XL_CHANNEL_FLAG_CANFD_ISO_SUPPORT = 0x80000000
 
 # structure for XL_RECEIVE_MSG, XL_TRANSMIT_MSG
 class s_xl_can_msg(ctypes.Structure):
@@ -54,11 +71,50 @@ class s_xl_can_msg(ctypes.Structure):
                 ('dlc', ctypes.c_ushort), ('res1', XLuint64),
                 ('data', ctypes.c_ubyte * MAX_MSG_LEN), ('res2', XLuint64)]
 
+
+
+class s_xl_can_ev_error(ctypes.Structure):
+	_fields_ = [('errorCode', ctypes.c_ubyte), ('reserved', ctypes.c_ubyte * 95)]
+
+class s_xl_can_ev_chip_state(ctypes.Structure):
+	_fields_ = [('busStatus', ctypes.c_ubyte), ('txErrorCounter', ctypes.c_ubyte),
+                ('rxErrorCounter', ctypes.c_ubyte),('reserved', ctypes.c_ubyte),
+                ('reserved0', ctypes.c_uint)]
+
+class s_xl_can_ev_sync_pulse(ctypes.Structure):
+	_fields_ = [('triggerSource', ctypes.c_uint), ('reserved', ctypes.c_uint),
+                ('time', XLuint64)]
+	
 # BASIC bus message structure
 class s_xl_tag_data(ctypes.Union):
     _fields_ = [('msg', s_xl_can_msg)]
 
+# CAN FD messages
+class s_xl_can_ev_rx_msg(ctypes.Structure):
+	_fields_ = [('canId', ctypes.c_uint), ('msgFlags', ctypes.c_uint),
+                ('crc', ctypes.c_uint), ('reserved1', ctypes.c_ubyte * 12),
+				('totalBitCnt', ctypes.c_ushort), ('dlc', ctypes.c_ubyte),
+                ('reserved', ctypes.c_ubyte * 5), ('data', ctypes.c_ubyte * XL_CAN_MAX_DATA_LEN)]
+				
+class s_xl_can_ev_tx_request(ctypes.Structure):
+	_fields_ = [('canId', ctypes.c_uint), ('msgFlags', ctypes.c_uint),
+                ('dlc', ctypes.c_ubyte),('txAttemptConf', ctypes.c_ubyte),
+                ('reserved', ctypes.c_ushort), ('data', ctypes.c_ubyte * XL_CAN_MAX_DATA_LEN)]
 
+class s_xl_can_tx_msg(ctypes.Structure):
+    _fields_ = [('canId', ctypes.c_uint), ('msgFlags', ctypes.c_uint),
+                ('dlc', ctypes.c_ubyte), ('reserved', ctypes.c_ubyte * 7),
+                ('data', ctypes.c_ubyte * XL_CAN_MAX_DATA_LEN)]
+
+class s_rxTagData(ctypes.Union):
+	_fields_ = [('canRxOkMsg', s_xl_can_ev_rx_msg), ('canTxOkMsg', s_xl_can_ev_rx_msg),
+                ('canTxRequest', s_xl_can_ev_tx_request),('canError', s_xl_can_ev_error),
+                ('canChipState', s_xl_can_ev_chip_state),('canSyncPulse', s_xl_can_ev_sync_pulse)]
+
+class s_txTagData(ctypes.Union):
+	_fields_ = [('canMsg', s_xl_can_tx_msg)]
+
+# BASIC events				
 XLeventTag = ctypes.c_ubyte
 
 class XLevent(ctypes.Structure):
@@ -67,6 +123,27 @@ class XLevent(ctypes.Structure):
                 ('flags', ctypes.c_ubyte), ('reserved', ctypes.c_ubyte),
                 ('timeStamp', XLuint64), ('tagData', s_xl_tag_data)]
 
+# CAN FD events				
+class XLcanRxEvent(ctypes.Structure):
+    _fields_ = [('size',ctypes.c_int),('tag', ctypes.c_ushort),
+                ('chanIndex', ctypes.c_ubyte),('reserved', ctypes.c_ubyte),
+                ('userHandle', ctypes.c_int),('flagsChip', ctypes.c_ushort),
+                ('reserved0', ctypes.c_ushort),('reserved1', XLuint64),
+                ('timeStamp', XLuint64),('tagData', s_rxTagData)]
+
+class XLcanTxEvent(ctypes.Structure):
+    _fields_ = [('tag', ctypes.c_ushort), ('transId', ctypes.c_ushort),
+                ('chanIndex', ctypes.c_ubyte), ('reserved', ctypes.c_ubyte * 3),
+                ('tagData', s_txTagData)]
+
+# CAN FD configuration structure
+class XLcanFdConf(ctypes.Structure):
+    _fields_ = [('arbitrationBitRate', ctypes.c_uint), ('sjwAbr', ctypes.c_uint),
+                ('tseg1Abr', ctypes.c_uint), ('tseg2Abr', ctypes.c_uint),
+                ('dataBitRate', ctypes.c_uint), ('sjwDbr', ctypes.c_uint),
+                ('tseg1Dbr', ctypes.c_uint), ('tseg2Dbr', ctypes.c_uint),
+                ('reserved', ctypes.c_uint * 2)]
+				
 # driver status
 XLstatus = ctypes.c_short
 
@@ -150,12 +227,24 @@ xlDeactivateChannel.argtypes = [XLportHandle, XLaccess]
 xlDeactivateChannel.restype = XLstatus
 xlDeactivateChannel.errcheck = check_status
 
+xlCanFdSetConfiguration = _xlapi_dll.xlCanFdSetConfiguration
+xlCanFdSetConfiguration.argtypes = [XLportHandle, XLaccess, ctypes.POINTER(XLcanFdConf)]
+xlCanFdSetConfiguration.restype = XLstatus
+xlCanFdSetConfiguration.errcheck = check_status
+
 xlReceive = _xlapi_dll.xlReceive
 xlReceive.argtypes = [
     XLportHandle, ctypes.POINTER(ctypes.c_uint), ctypes.POINTER(XLevent)
 ]
 xlReceive.restype = XLstatus
 xlReceive.errcheck = check_status
+
+xlCanReceive = _xlapi_dll.xlCanReceive
+xlCanReceive.argtypes = [
+    XLportHandle, ctypes.POINTER(XLcanRxEvent)
+]
+xlCanReceive.restype = XLstatus
+xlCanReceive.errcheck = check_status
 
 xlGetErrorString = _xlapi_dll.xlGetErrorString
 xlGetErrorString.argtypes = [XLstatus]
@@ -172,6 +261,13 @@ xlCanTransmit.argtypes = [
 ]
 xlCanTransmit.restype = XLstatus
 xlCanTransmit.errcheck = check_status
+
+xlCanTransmitEx = _xlapi_dll.xlCanTransmitEx
+xlCanTransmitEx.argtypes = [
+    XLportHandle, XLaccess, ctypes.c_uint, ctypes.POINTER(ctypes.c_uint), ctypes.POINTER(XLcanTxEvent)
+]
+xlCanTransmitEx.restype = XLstatus
+xlCanTransmitEx.errcheck = check_status
 
 xlCanFlushTransmitQueue = _xlapi_dll.xlCanFlushTransmitQueue
 xlCanFlushTransmitQueue.argtypes = [XLportHandle, XLaccess]
