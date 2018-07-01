4591b51584f73525c0a16393b9592245c965eab9
Felix Divo <felix.divo@gmail.com>
2018-07-01 19:01:25 +0200

    explicitly add python as the standard domain

--- doc/conf.py
+++ doc/conf.py
@@ -28,6 +28,8 @@ release = can.__version__
 
 # -- General configuration -----------------------------------------------------
 
+primary_domain = 'py'
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
