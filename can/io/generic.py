3368c24e702017f68603da2cec1897681171a0dc
Felix Divo <felix.divo@gmail.com>
2018-07-03 16:24:06 +0200

    docs

--- can/io/generic.py
+++ can/io/generic.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-Contains generic classes for file IO.
+Contains a generic class for file IO.
 """
 
 from abc import ABCMeta, abstractmethod
@@ -12,14 +12,20 @@ from can import Listener
 
 class BaseIOHandler(object):
     """A generic file handler that can be used for reading and writing.
+
+    Can be used as a context manager.
     """
 
     __metaclass__ = ABCMeta
 
     def __init__(self, open_file, filename='can_data', mode='Urt'):
         """
-        TODO docs
+        :param bool open_file: opens a file if set to True
+        :param str filename: the path/filename of the file to open
+        :param str mode: the mode that should be used to open the file, see
+                         :func:`builtin.open`
         """
+        super(BaseIOHandler, self).__init__() # for multiple inheritance
         if open_file:
             self.file = open(filename, mode)
 
