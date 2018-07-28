b578688c6faefd5205faf1d38edce5cfb6f7bde2
Felix Divo <felixdivo@users.noreply.github.com>
2018-07-28 14:20:26 +0200

    Made virtual_can_demo.py thread safe

--- examples/virtual_can_demo.py
+++ examples/virtual_can_demo.py
@@ -5,27 +5,25 @@
 This demo creates multiple processes of Producers to spam a socketcan bus.
 """
 
-import time
-import logging
-import concurrent.futures
+from time import sleep
+from concurrent.futures import ProcessPoolExecutor
 
 import can
 
 
 def producer(id):
-    """:param id: Spam the bus with messages including the data id."""
+    """Spam the bus with messages including the data id.
+    :param int id: the id of the thread/process
+    """
 
-    bus = can.interface.Bus(bustype='socketcan', channel='vcan0')
-    for i in range(16):
-        msg = can.Message(arbitration_id=0x0cf02200+id, data=[id, i, 0, 1, 3, 1, 4, 1])
-        bus.send(msg)
-
-    # TODO Issue #3: Need to keep running to ensure the writing threads stay alive. ?
-    time.sleep(2)
+    with can.ThreadSafeBus(bustype='socketcan', channel='vcan0') as bus:
+        for i in range(16):
+            msg = can.Message(arbitration_id=0x0cf02200+id, data=[id, i, 0, 1, 3, 1, 4, 1])
+            bus.send(msg)
+        sleep(1.0)
 
 if __name__ == "__main__":
-    #logging.getLogger('').setLevel(logging.DEBUG)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+    with ProcessPoolExecutor(max_workers=4) as executor:
         executor.map(producer, range(5))
 
-time.sleep(2)
+sleep(1.0)
