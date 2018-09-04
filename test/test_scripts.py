d998c4faef816da3466340e102c0228d4ab86bfd
Felix Divo <felix.divo@gmail.com>
2018-09-04 13:49:09 +0200

    better tests for scripts

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-This module tests that the scripts are callable.
+This module tests that the scripts are all callable.
 """
 
 from __future__ import absolute_import
@@ -11,31 +11,54 @@ import subprocess
 import unittest
 
 
-#: these are commands that should return successfully
-COMMANDS = (
-    "can_logger.py --help",
-    "python -m can.logger --help",
-    "python -m can.scripts.logger --help",
-
-    "can_player.py --help",
-    "python -m can.player --help",
-    "python -m can.scripts.player --help",
-
-    # TODO add #390
-)
-
-
-class TestCanScripts(unittest.TestCase):
+class TestCanScript(object):
 
     def do_commands_exist(self):
         """This test calls each scripts once and veifies that the help
         can be read without any errors.
         """
-        for command in COMMANDS:
+        for command in self._commands():
             try:
                 subprocess.check_output(COMMANDS.spli(), stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 self.fail('Calling "{}" failed:\n{}'.format(command, e.output))
+
+    def does_not_crash(self):
+        # test import
+        module = self._import()
+        # test main method
+        module.main()
+
+
+class TestLoggerScript(unittest.TestCase, TestCanScript):
+
+    def _commands(self):
+        return (
+            "can_logger.py --help",
+            "python -m can.logger --help",
+            "python -m can.scripts.logger --help"
+        )
+
+    def _import(self):
+        import can.scripts.logger as module
+        return module
+
+
+class TestPlayerScript(unittest.TestCase, TestCanScript):
+
+    def _commands(self):
+        return (
+            "can_player.py --help",
+            "python -m can.player --help",
+            "python -m can.scripts.player --help"
+        )
+
+    def _import(self):
+        import can.scripts.player as module
+        return module
+
+
+# TODO add #390
 
 
 if __name__ == '__main__':
