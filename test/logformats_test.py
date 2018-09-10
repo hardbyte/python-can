edb2b6e7044c9e86a3b3eba4d06d13dd7fd28044
Felix Divo <felix.divo@gmail.com>
2018-09-10 16:14:37 +0200

    make timestamp comparisoun a bit more forgiving

--- test/logformats_test.py
+++ test/logformats_test.py
@@ -320,7 +320,7 @@ class ReaderWriterTest(unittest.TestCase):
         else:
             t1 = round(message_1.timestamp) if self.round_timestamps else message_1.timestamp
             t2 = round(message_2.timestamp) if self.round_timestamps else message_2.timestamp
-            self.assertAlmostEqual(message_2.timestamp, message_1.timestamp, places=6,
+            self.assertAlmostEqual(message_2.timestamp, message_1.timestamp, delta=0.0001,
                 msg="message timestamps are not almost_equal ({!r} !~= {!r}{})"
                     .format(message_1.timestamp, message_2.timestamp,
                             "; rounded" if self.round_timestamps else ""))
