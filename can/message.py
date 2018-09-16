eee819f8e8200f11325d26539928b72c7d0d0697
Felix Divo <felix.divo@gmail.com>
2018-09-16 02:19:05 +0200

    update deprecation notes

--- can/message.py
+++ can/message.py
@@ -43,8 +43,8 @@ class Message(object):
 
     def __getattr__(self, key):
         # TODO keep this for a version, in order to not break old code
-        # this entire method (as well as the _dict attribute in __slots__ and the __setattr__ method) can be removed
-        # in the next major version after 2.3
+        # this entire method (as well as the _dict attribute in __slots__ and the __setattr__ method)
+        # can be removed in 3.0
         # this method is only called if the attribute was not found elsewhere, like in __slots__
         try:
             warnings.warn("Custom attributes of messages are deprecated and will be removed in the next major version", DeprecationWarning)
@@ -62,11 +62,13 @@ class Message(object):
 
     @property
     def id_type(self):
+        # TODO remove in 3.0
         warnings.warn("Message.id_type is deprecated, use is_extended_id", DeprecationWarning)
         return self.is_extended_id
 
     @id_type.setter
     def id_type(self, value):
+        # TODO remove in 3.0
         warnings.warn("Message.id_type is deprecated, use is_extended_id", DeprecationWarning)
         self.is_extended_id = value
 
