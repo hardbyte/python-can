247df08ba1bed4b3eb675d768f26e230f2fe1eee
Christian Sandberg <christiansandberg@me.com>
2018-10-02 21:57:31 +0200

    Fix some slcan issues
    Allow partial messages to be received
    Fix DLC for remote frames
    Add unit test

--- can/interfaces/slcan.py
+++ can/interfaces/slcan.py
@@ -47,7 +47,10 @@ class slcanBus(BusABC):
 
     _SLEEP_AFTER_SERIAL_OPEN = 2  # in seconds
 
+    LINE_TERMINATOR = b'\r'
+
     def __init__(self, channel, ttyBaudrate=115200, bitrate=None,
+                 sleep_after_open=_SLEEP_AFTER_SERIAL_OPEN,
                  rtscts=False, **kwargs):
         """
         :param str channel:
@@ -59,6 +62,8 @@ class slcanBus(BusABC):
             Bitrate in bit/s
         :param float poll_interval:
             Poll interval in seconds when reading messages
+        :param float sleep_after_open:
+            Time to wait in seconds after opening serial connection
         :param bool rtscts:
             turn hardware handshake (RTS/CTS) on and off
         """
@@ -72,7 +77,9 @@ class slcanBus(BusABC):
         self.serialPortOrig = serial.serial_for_url(
             channel, baudrate=ttyBaudrate, rtscts=rtscts)
 
-        time.sleep(self._SLEEP_AFTER_SERIAL_OPEN)
+        self._buffer = bytearray()
+
+        time.sleep(sleep_after_open)
 
         if bitrate is not None:
             self.close()
@@ -87,9 +94,7 @@ class slcanBus(BusABC):
                                        bitrate=None, rtscts=False, **kwargs)
 
     def write(self, string):
-        if not string.endswith('\r'):
-            string += '\r'
-        self.serialPortOrig.write(string.encode())
+        self.serialPortOrig.write(string.encode() + self.LINE_TERMINATOR)
         self.serialPortOrig.flush()
 
     def open(self):
@@ -107,12 +112,20 @@ class slcanBus(BusABC):
         extended = False
         frame = []
 
-        readStr = self.serialPortOrig.read_until(b'\r')
+        # Read everything that is already available
+        waiting = self.serialPortOrig.read(self.serialPortOrig.in_waiting)
+        self._buffer += waiting
 
-        if not readStr:
-            return None, False
-        else:
-            readStr = readStr.decode()
+        # Check if a complete message has been received
+        pos = self._buffer.find(self.LINE_TERMINATOR)
+        if pos == -1:
+            # Keep reading...
+            self._buffer += self.serialPortOrig.read_until(self.LINE_TERMINATOR)
+            pos = self._buffer.find(self.LINE_TERMINATOR)
+
+        if pos != -1:
+            readStr = self._buffer[0:pos].decode()
+            del self._buffer[0:pos]
             if readStr[0] == 'T':
                 # extended frame
                 canId = int(readStr[1:9], 16)
@@ -129,45 +142,46 @@ class slcanBus(BusABC):
             elif readStr[0] == 'r':
                 # remote frame
                 canId = int(readStr[1:4], 16)
+                dlc = int(readStr[4])
                 remote = True
             elif readStr[0] == 'R':
                 # remote extended frame
                 canId = int(readStr[1:9], 16)
+                dlc = int(readStr[9])
                 extended = True
                 remote = True
 
             if canId is not None:
                 msg = Message(arbitration_id=canId,
-                              extended_id=extended,
+                              is_extended_id=extended,
                               timestamp=time.time(),   # Better than nothing...
                               is_remote_frame=remote,
                               dlc=dlc,
                               data=frame)
                 return msg, False
-            else:
-                return None, False
+        return None, False
 
-    def send(self, msg, timeout=0):
+    def send(self, msg, timeout=None):
         if timeout != self.serialPortOrig.write_timeout:
             self.serialPortOrig.write_timeout = timeout
 
         if msg.is_remote_frame:
             if msg.is_extended_id:
-                sendStr = "R%08X0" % (msg.arbitration_id)
+                sendStr = "R%08X%d" % (msg.arbitration_id, msg.dlc)
             else:
-                sendStr = "r%03X0" % (msg.arbitration_id)
+                sendStr = "r%03X%d" % (msg.arbitration_id, msg.dlc)
         else:
             if msg.is_extended_id:
                 sendStr = "T%08X%d" % (msg.arbitration_id, msg.dlc)
             else:
                 sendStr = "t%03X%d" % (msg.arbitration_id, msg.dlc)
 
-            for i in range(0, msg.dlc):
-                sendStr += "%02X" % msg.data[i]
+            sendStr += "".join(["%02X" % b for b in msg.data])
         self.write(sendStr)
 
     def shutdown(self):
         self.close()
+        self.serialPortOrig.close()
 
     def fileno(self):
         if hasattr(self.serialPortOrig, 'fileno'):
