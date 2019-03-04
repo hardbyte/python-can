4be2233786b3098a27785a03ecbe667b233046c6
Felix Divo <felixdivo@users.noreply.github.com>
2019-03-04 04:31:25 +0100

    Improve USB2CAN interface (#511)
    * apply fixes from https://gist.github.com/acolomb/945cc5e5b29ced411f1df5b5f9c4a4d0
    * simplify find_serial_devices()
    * fix sphinx warning about indentation

--- can/interfaces/usb2can/usb2canabstractionlayer.py
+++ can/interfaces/usb2can/usb2canabstractionlayer.py
@@ -5,10 +5,13 @@ This wrapper is for windows or direct access via CANAL API.
 Socket CAN is recommended under Unix/Linux systems.
 """
 
-import can
+from __future__ import division, print_function, absolute_import
+
 from ctypes import *
 from struct import *
 import logging
+
+import can
 
 log = logging.getLogger('can.usb2can')
 
@@ -17,12 +20,16 @@ flags = c_ulong
 pConfigureStr = c_char_p
 handle = c_long
 timeout = c_ulong
-filter = c_ulong
+filter_t = c_ulong
 
 # flags mappings
 IS_ERROR_FRAME = 4
 IS_REMOTE_FRAME = 2
 IS_ID_TYPE = 1
+
+CANAL_ERROR_SUCCESS = 0
+CANAL_ERROR_RCV_EMPTY = 19
+CANAL_ERROR_TIMEOUT = 32
 
 
 class CanalStatistics(Structure):
@@ -59,21 +66,45 @@ class Usb2CanAbstractionLayer:
     """A low level wrapper around the usb2can library.
 
     Documentation: http://www.8devices.com/media/products/usb2can/downloads/CANAL_API.pdf
-
     """
-    def __init__(self):
-        self.__m_dllBasic = windll.LoadLibrary("usb2can.dll")
+
+    def __init__(self, dll="usb2can.dll"):
+        """
+        :type dll: str or path-like
+        :param dll (optional): the path to the usb2can DLL to load
+        :raises OSError: if the DLL could not be loaded
+        """
+        self.__m_dllBasic = windll.LoadLibrary(dll)
 
         if self.__m_dllBasic is None:
-            log.warning('DLL failed to load')
+            log.warning('DLL failed to load at path: {}'.format(dll))
 
-    def open(self, pConfigureStr, flags):
+    def open(self, configuration, flags):
+        """
+        Opens a CAN connection using `CanalOpen()`.
+
+        :param str configuration: the configuration: "device_id; baudrate"
+        :param int flags: the flags to be set
+
+        :raises can.CanError: if any error occurred
+        :returns: Nothing
+        """
         try:
-            res = self.__m_dllBasic.CanalOpen(pConfigureStr, flags)
-            return res
-        except:
-            log.warning('Failed to open')
-            raise
+            # we need to convert this into bytes, since the underlying DLL cannot
+            # handle non-ASCII configuration strings
+            config_ascii = configuration.encode('ascii', 'ignore')
+            result = self.__m_dllBasic.CanalOpen(config_ascii, flags)
+        except Exception as ex:
+            # catch any errors thrown by this call and re-raise
+            raise can.CanError('CanalOpen() failed, configuration: "{}", error: {}'
+                               .format(configuration, ex))
+        else:
+            # any greater-than-zero return value indicates a success
+            # (see https://grodansparadis.gitbooks.io/the-vscp-daemon/canal_interface_specification.html)
+            # raise an error if the return code is <= 0
+            if result <= 0:
+                raise can.CanError('CanalOpen() failed, configuration: "{}", return code: {}'
+                                   .format(configuration, result))
 
     def close(self, handle):
         try:
