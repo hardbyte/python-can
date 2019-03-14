09d6c1f515abf6dd148bdb72da9acd70a3037b3e
Felix Divo <felix.divo@gmail.com>
2019-03-14 15:09:37 +0100

    fix missing dependency on python 3.6+

--- setup.py
+++ setup.py
@@ -101,8 +101,8 @@ setup(
     python_requires=">=2.7,!=3.0,!=3.1,!=3.2,!=3.3",
     install_requires=[
         'wrapt~=1.10',
+        'aenum',
         'typing;python_version<"3.5"',
-        'aenum;python_version<"3.6"',
         'windows-curses;platform_system=="Windows"',
     ],
     extras_require=extras_require,
