693f93cc9371c34e4238a80c405822f7f94d6c8c
Karl <karlding@users.noreply.github.com>
2019-05-09 20:10:18 -0700

    Fix stuct packing in build_bcm_header on 32-bit
    Previously build_bcm_header relied upon adding a "0q" at the end of the
    struct format string in order to force a 8-byte wide alignment on the
    struct, in order to interop with the structure defined in the Linux
    headers:
    
        struct bcm_msg_head {
          __u32 opcode;
          __u32 flags;
          __u32 count;
          struct bcm_timeval ival1, ival2;
          canid_t can_id;
          __u32 nframes;
          struct can_frame frames[0];
        };
    
    The Python code assumed that alignof(long long) == 8 bytes in order to
    accomplish this. However, on certain 32-bit platforms, this assumption
    is not true, and alignof(long long) == 4.
    
    As the struct module does not provide logic to derive alignments of its
    types, this changes the packing logic to use ctypes. This exposes the
    alignment of the struct members, allowing us to determine whether padding
    bytes are necessary.
    
    Fixes #470

--- can/interfaces/socketcan/socketcan.py
+++ can/interfaces/socketcan/socketcan.py
@@ -118,6 +118,41 @@ def build_can_frame(msg):
     return CAN_FRAME_HEADER_STRUCT.pack(can_id, msg.dlc, flags) + data
 
 
+def bcm_header_factory(fields, alignment=8):
+    curr_stride = 0
+    results = []
+    pad_index = 0
+    for field in fields:
+        field_alignment = ctypes.alignment(field[1])
+        field_size = ctypes.sizeof(field[1])
+
+        # If the current stride index isn't a multiple of the alignment
+        # requirements of this field, then we must add padding bytes until we
+        # are aligned
+        while curr_stride % field_alignment != 0:
+            results.append(("pad_{}".format(pad_index), ctypes.c_uint8))
+            pad_index += 1
+            curr_stride += 1
+
+        # Now can it fit?
+        # Example: If this is 8 bytes and the type requires 4 bytes alignment
+        # then we can only fit when we're starting at 0. Otherwise, we will
+        # split across 2 strides.
+        #
+        # | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+        results.append(field)
+        curr_stride += field_size
+
+    # Add trailing padding to align to a multiple of the largest scalar member
+    # in the structure
+    while curr_stride % alignment != 0:
+        results.append(("pad_{}".format(pad_index), ctypes.c_uint8))
+        pad_index += 1
+        curr_stride += 1
+
+    return type("BcmMsgHead", (ctypes.Structure,), {"_fields_": results})
+
+
 def build_bcm_header(opcode, flags, count, ival1_seconds, ival1_usec, ival2_seconds, ival2_usec, can_id, nframes):
     # == Must use native not standard types for packing ==
     # struct bcm_msg_head {
@@ -127,18 +162,31 @@ def build_bcm_header(opcode, flags, count, ival1_seconds, ival1_usec, ival2_seco
     #     struct timeval ival1, ival2; ->  llll ...
     #     canid_t can_id; -> I
     #     __u32 nframes; -> I
-    bcm_cmd_msg_fmt = "@3I4l2I0q"
 
-    return struct.pack(bcm_cmd_msg_fmt,
-                       opcode,
-                       flags,
-                       count,
-                       ival1_seconds,
-                       ival1_usec,
-                       ival2_seconds,
-                       ival2_usec,
-                       can_id,
-                       nframes)
+    fields = [
+        ("opcode", ctypes.c_uint32),
+        ("flags", ctypes.c_uint32),
+        ("count", ctypes.c_uint32),
+        ("ival1_tv_sec", ctypes.c_long),
+        ("ival1_tv_usec", ctypes.c_long),
+        ("ival2_tv_sec", ctypes.c_long),
+        ("ival2_tv_usec", ctypes.c_long),
+        ("can_id", ctypes.c_uint32),
+        ("nframes", ctypes.c_uint32),
+    ]
+    BcmMsgHead = bcm_header_factory(fields)
+    result = BcmMsgHead(
+        opcode=opcode,
+        flags=flags,
+        count=count,
+        ival1_tv_sec=ival1_seconds,
+        ival1_tv_usec=ival1_usec,
+        ival2_tv_sec=ival2_seconds,
+        ival2_tv_usec=ival2_usec,
+        can_id=can_id,
+        nframes=nframes,
+    )
+    return ctypes.string_at(ctypes.addressof(result), ctypes.sizeof(result))
 
 
 def build_bcm_tx_delete_header(can_id, flags):
