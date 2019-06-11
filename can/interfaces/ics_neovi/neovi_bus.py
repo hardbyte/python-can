f68de2db1a03f0f2234b8779250c373ff9ef997a
Pierre-Luc Tessier Gagné <ptessie3@ford.com>
2019-06-11 08:51:25 -0400

    Move neoVI open lock to module level

--- can/interfaces/ics_neovi/neovi_bus.py
+++ can/interfaces/ics_neovi/neovi_bus.py
@@ -15,8 +15,9 @@ import os
 import tempfile
 from collections import deque
 
-from can import Message, CanError, BusABC
 from filelock import FileLock
+
+from can import Message, CanError, BusABC
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +29,11 @@ except ImportError as ie:
         "python-ics module installed!: %s", ie
     )
     ics = None
+
+
+# Use inter-process mutex to prevent concurrent device open.
+# When neoVI server is enabled, there is an issue with concurrent device open.
+open_lock = FileLock(os.path.join(tempfile.gettempdir(), "neovi.lock"))
 
 
 class ICSApiError(CanError):
@@ -122,11 +128,6 @@ class NeoViBus(BusABC):
         serial = kwargs.get('serial')
         self.dev = self._find_device(type_filter, serial)
 
-        # Use inter-process mutex to prevent concurrent device open.
-        # When neoVI server is enabled, there is an issue with concurrent
-        # device open.
-        open_lock = FileLock(
-            os.path.join(tempfile.gettempdir(), 'neovi.lock'))
         with open_lock:
             ics.open_device(self.dev)
 
