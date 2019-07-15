9f170be15ea49365bcdd6d442a7c4f84796ac301
Seemann Jochen (CC-DA/ECU2) <Jochen.Seemann@de.bosch.com>
2019-07-15 12:43:55 +0200

    fix unvalid syntax

--- can/interfaces/vector/canlib.py
+++ can/interfaces/vector/canlib.py
@@ -486,7 +486,7 @@ class VectorBus(BusABC):
                     "interface": "vector",
                     "app_name": None,
                     "channel": channel_config.channelIndex,
-                    'supports_fd': bool(channel_config.channelBusCapabilities & vxlapi.XL_CHANNEL_FLAG_CANFD_ISO_SUPPORT)}
+                    'supports_fd': bool(channel_config.channelBusCapabilities & vxlapi.XL_CHANNEL_FLAG_CANFD_ISO_SUPPORT)
                 }
             )
         return configs
