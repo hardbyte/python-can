41e592c4e776294f9d7b42467b99824ff5bfa09b
Karl <karlding@users.noreply.github.com>
2019-08-01 01:34:06 -0700

    Add typing annotations for can.message
    This adds typing annotations for functions in can.message. Currently the
    typing annotation for the Message data is incomplete due to upstream not
    implementing the buffer protocol. Since can.Message attempts to cast the
    data to a bytearray via bytearray(), the Message data can be any type
    that bytearray() supports.
    
    In addition, this remove the redundant typing information that was
    previously in the docstring, since we now have sphinx-autodoc-typehints
    to generate the types for the docs from the annotations in the function
    signature.
    
    This works towards PEP 561 compatibility.

--- can/typechecking.py
+++ can/typechecking.py
@@ -9,5 +9,12 @@ CanFilter = mypy_extensions.TypedDict(
 )
 CanFilters = typing.Iterable[CanFilter]
 
+# TODO: Once buffer protocol support lands in typing, we should switch to that,
+# since can.message.Message attempts to call bytearray() on the given data, so
+# this should have the same typing info.
+#
+# See: https://github.com/python/typing/issues/593
+CanData = typing.Union[bytes, bytearray, int, typing.Iterable[int]]
+
 # Used for the Abstract Base Class
 Channel = typing.Union[int, str]
