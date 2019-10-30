4ed0472d5e59ca80b3250c3d7b3d094d35d33010
tamenol <37591107+tamenol@users.noreply.github.com>
2019-10-30 14:09:58 +0100

    Revert "Revert "check driver availability""
    This reverts commit 0227bd60ccf090596e668239080687a0da2f0d15.

--- can/interfaces/pcan/basic.py
+++ can/interfaces/pcan/basic.py
@@ -14,6 +14,7 @@ from ctypes import *
 from string import *
 import platform
 import logging
+import winreg
 
 logger = logging.getLogger("can.pcan")
 
@@ -498,9 +499,16 @@ class PCANBasic:
     """
 
     def __init__(self):
-        # Loads the PCANBasic.dll
+        # Loads the PCANBasic.dll and checks if driver is available
         if platform.system() == "Windows":
             self.__m_dllBasic = windll.LoadLibrary("PCANBasic")
+            aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+            try:
+                aKey = winreg.OpenKey(aReg, r"SOFTWARE\PEAK-System\PEAK-Drivers")
+                winreg.CloseKey(aKey)
+            except WindowsError:
+                logger.error("Exception: The PEAK-driver couldn't be found!")
+            winreg.CloseKey(aReg)
         elif platform.system() == "Darwin":
             self.__m_dllBasic = cdll.LoadLibrary("libPCBUSB.dylib")
         else:
