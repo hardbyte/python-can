ac90f6dfba0a7d9c4b76949d132bbd029912cb71
tamenol <37591107+tamenol@users.noreply.github.com>
2019-11-05 09:16:18 +0100

    revert to older commit

--- can/interfaces/pcan/basic.py
+++ can/interfaces/pcan/basic.py
@@ -14,7 +14,6 @@ from ctypes import *
 from string import *
 import platform
 import logging
-import winreg
 
 logger = logging.getLogger("can.pcan")
 
@@ -499,16 +498,9 @@ class PCANBasic:
     """
 
     def __init__(self):
-        # Loads the PCANBasic.dll and checks if driver is available
+        # Loads the PCANBasic.dll
         if platform.system() == "Windows":
             self.__m_dllBasic = windll.LoadLibrary("PCANBasic")
-            aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-            try:
-                aKey = winreg.OpenKey(aReg, r"SOFTWARE\PEAK-System\PEAK-Drivers")
-                winreg.CloseKey(aKey)
-            except WindowsError:
-                logger.error("Exception: The PEAK-driver couldn't be found!")
-            winreg.CloseKey(aReg)
         elif platform.system() == "Darwin":
             self.__m_dllBasic = cdll.LoadLibrary("libPCBUSB.dylib")
         else:
