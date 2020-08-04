0c34e5070ccaa3c89d6c1893370083943f0d5390
Karl <karlding@users.noreply.github.com>
2020-08-03 22:14:45 -0700

    Fix RecursionError in Message.__getattr__
    __getattr__ incorrectly assumes that self._dict always exists. However,
    if self._dict doesn't exist, the function attempts to call __getattr__
    again, which results in infinite recursion when serializing the object
    via pickle.
    
    This fixes the implementation of __getattr__ and adds a test to exercise
    pickling/unpickling the Message.
    
    Fixes #804

--- can/message.py
+++ can/message.py
@@ -53,7 +53,9 @@ class Message(object):
         # TODO keep this for a version, in order to not break old code
         # this entire method (as well as the _dict attribute in __slots__ and the __setattr__ method)
         # can be removed in 4.0
-        # this method is only called if the attribute was not found elsewhere, like in __slots__
+        # this method is only called if the attribute was not found elsewhere, like in __slots_
+        if key not in self.__slots__:
+            raise AttributeError
         try:
             warnings.warn("Custom attributes of messages are deprecated and will be removed in 4.0", DeprecationWarning)
             return self._dict[key]
