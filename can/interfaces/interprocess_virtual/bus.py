122830ea7f8bd2becf575264c4bb51d8a3ab7500
Felix Divo <felix.divo@gmail.com>
2020-11-19 10:48:43 +0100

    cleanup

--- can/interfaces/interprocess_virtual/bus.py
+++ can/interfaces/interprocess_virtual/bus.py
@@ -21,16 +21,34 @@ except ImportError:
 
 
 class InterprocessVirtualBus(BusABC):
-    """A virtual interface that allows to communicate between multiple processes using Multicast IP.
+    """A virtual interface that allows to communicate between multiple processes using UDP over Multicast IP.
+
+    It supports IPv4 and IPv6, specified via the channel. This means that the channel is a multicast IP
+    address. You can also specify the port and TTL (*time to live*).
+
+    This bus does not support based on filtering message IDs on the kernel level.
     """
 
     DEFAULT_GROUP_IPv4 = '225.0.0.250'
     DEFAULT_GROUP_IPv6 = 'ff15:7079:7468:6f6e:6465:6d6f:6d63:6173'
 
     def __init__(self, channel=DEFAULT_GROUP_IPv6, port=43113, ttl=1,
-                 receive_own_messages=False, **kwargs):
-        super(InterprocessVirtualBus, self).__init__(channel, **kwargs)
+                 receive_own_messages=False, is_fd=True, **kwargs):
+        """Creates a new interprocess virtual bus.
 
+        :param channel: An IPv4/IPv6 multicast address.
+                        See `Wikipedia <https://en.wikipedia.org/wiki/IP_multicast>`__ for more details.
+        :param receive_own_messages:
+            If transmitted messages should also be received by this bus.
+        :param fd:
+            If CAN-FD frames should be supported. If set to false, an error will be raised upon sending such a
+            frame and such received frames will be ignored.
+        :param can_filters:
+            See :meth:`can.BusABC.set_filters`.
+        """
+        super().__init__(channel, **kwargs)
+
+        self.is_fd = is_fd
         self.multicast = GeneralPurposeMulticastBus(channel, port, ttl, receive_own_messages)
 
     def _recv_internal(self, timeout):
@@ -41,10 +59,16 @@ class InterprocessVirtualBus(BusABC):
         data, _, timestamp = result
         can_message = unpack_message(data, replace={'timestamp': timestamp})
 
+        if not self.is_fd and can_message.is_fd:
+            return None, False
+
         return can_message, False
 
-    def send(self, msg, timeout=None):
-        data = pack_message(msg)
+    def send(self, message, timeout=None):
+        if not self.is_fd and message.is_fd:
+            raise RuntimeError("cannot send FD message over bus with CAN FD disabled")
+
+        data = pack_message(message)
         self.multicast.send(data)
 
     def shutdown(self):
@@ -65,8 +89,7 @@ class InterprocessVirtualBus(BusABC):
 
 
 class GeneralPurposeMulticastBus:
-    """A general purpose send and receive handler for multicast over IP.
-    """
+    """A general purpose send and receive handler for multicast over IP/UDP."""
 
     def __init__(self, group, port, ttl, receive_own_messages, max_buffer=4096):
         self.group = group
@@ -76,14 +99,14 @@ class GeneralPurposeMulticastBus:
         self.max_buffer = max_buffer
 
         # Look up multicast group address in name server and find out IP version
-        self._addrinfo = socket.getaddrinfo(group, None)[0]
-        self.ip_version = 4 if self._addrinfo[0] == socket.AF_INET else 6
-        self._send_destination = (self._addrinfo[4][0], self.port)  # TODO might be replaceable
+        self._address_info = socket.getaddrinfo(group, None)[0]
+        self.ip_version = 4 if self._address_info[0] == socket.AF_INET else 6
+        self._send_destination = (self._address_info[4][0], self.port)  # TODO might be replaceable
 
         self._socket = self._create_socket()
 
     def _create_socket(self):
-        sock = socket.socket(self._addrinfo[0], socket.SOCK_DGRAM)
+        sock = socket.socket(self._address_info[0], socket.SOCK_DGRAM)
 
         # set TTL
         ttl_bin = struct.pack('@i', self.ttl)
@@ -100,7 +123,7 @@ class GeneralPurposeMulticastBus:
         sock.bind(('', self.port))
 
         # Join the multicast group
-        group_as_binary = socket.inet_pton(self._addrinfo[0], self._addrinfo[4][0])
+        group_as_binary = socket.inet_pton(self._address_info[0], self._address_info[4][0])
         if self.ip_version == 4:
             request = group_as_binary + struct.pack('=I', socket.INADDR_ANY)
             sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, request)
@@ -111,8 +134,7 @@ class GeneralPurposeMulticastBus:
         return sock
 
     def send(self, data, start=0, count=None):
-        """
-        Send this data to all participants.
+        """Send this data to all participants. This call blocks.
 
         :param data: the data to be sent as byte object,
                      or if size is not given (i.e. it is not zero or None) it must be
