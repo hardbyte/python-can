2c1d56a0950c84700b351201f82f8fe9ef04f5f3
Kevin Schlosser <drschlosser@hotmail.com>
2020-11-23 00:25:48 -0700

    extends the bit_timing.BitTiming class
    Adds calculations to provide timings with less input from the user.
    
    ```python
    import can
    btiming = can.BitTiming(500000, 8000000)
    print(btiming)
    print()
    
    btiming = can.BitTiming(500000, 8000000, sample_point=87.5)
    print(btiming)
    ```
    
    outputs
    
    ```
    500000 bits/s, sample point: 93.75%, BRP: 1, TSEG1: 14, TSEG2: 1, SJW: 1, BTR: 000Dh
    
    500000 bits/s, sample point: 87.50%, BRP: 1, TSEG1: 13, TSEG2: 2, SJW: 1, BTR: 001Ch
    ```
    
    Should we have the f_clock default to 8000000Hz if the bitrate has been supplied?
    I believe that most interfaces have a clock of 16000000Hz

--- can/bit_timing.py
+++ can/bit_timing.py
@@ -24,10 +24,18 @@ class BitTiming:
     """
 
     sync_seg = 1
+    # generator that produces all possible combinations of brp, sjw, tseg1 and tseg2
+    _calc = [
+        (can_brp, can_sjw, can_tseg1, can_tseg2)
+        for can_brp in range(1, 65)  # baudrate prescalar (1-64)
+        for can_sjw in range(1, 5)  # synchronization jump width (1-4)
+        for can_tseg1 in range(1, 17)  # prop_seg + phase_seg1 (1-16)
+        for can_tseg2 in range(1, 9)  # phase_seg2 (1-8)
+    ]
 
     def __init__(
         self,
-        bitrate: Optional[int] = None,
+        bitrate: Optional[Union[int, float]] = None,
         f_clock: Optional[int] = None,
         brp: Optional[int] = None,
         tseg1: Optional[int] = None,
@@ -36,9 +44,22 @@ class BitTiming:
         nof_samples: int = 1,
         btr0: Optional[int] = None,
         btr1: Optional[int] = None,
+        sample_point: Optional[Union[int, float]] = None
     ):
         """
-        :param int bitrate:
+        Calculates the timings needed for most interfaces
+
+        >>>import can
+        >>>from can.interfaces.canalystii import CANalystIIBus
+        >>>btiming = can.BitTiming(500000, f_clock=8000000)
+        >>>print(btiming)
+        500000 bits/s, sample point: 93.75%, BRP: 1, TSEG1: 14, TSEG2: 1, SJW: 1, BTR: 000Dh
+        >>>btiming = can.BitTiming(500000, f_clock=8000000, sample_point=87.5)
+        >>>print(btiming)
+        500000 bits/s, sample point: 87.50%, BRP: 1, TSEG1: 13, TSEG2: 2, SJW: 1, BTR: 001Ch
+        >>>can_interface = CANalystIIBus(0, Timing0=btiming.btr0, Timing1=btiming.btr1)
+
+        :param int, float bitrate:
             Bitrate in bits/s.
         :param int f_clock:
             The CAN system clock frequency in Hz.
@@ -64,6 +85,7 @@ class BitTiming:
         :param int btr1:
             The BTR1 register value used by many CAN controllers.
         """
+
         self._bitrate = bitrate
         self._brp = brp
         self._sjw = sjw
@@ -83,6 +105,89 @@ class BitTiming:
         if nof_samples not in (1, 3):
             raise ValueError("nof_samples must be 1 or 3")
 
+        self._sample_point = sample_point
+
+    def _calc_timings(self):
+        if self._f_clock is None:
+            raise ValueError('The f_clock is needed in order to calculate the timings')
+
+        close_low = 0
+        close_high = 99999999999999
+        high_sample = 0
+        low_sample = 0
+        low = (0, 0, 0, 0)
+        high = (0, 0, 0, 0)
+        sample = 0
+        res = None
+
+        for can_brp, can_sjw, can_tseg1, can_tseg2 in self._calc:
+            b = self._f_clock / (can_brp * (1 + can_tseg1 + can_tseg2))
+            s = ((can_tseg1 + 1) / (1 + can_tseg1 + can_tseg2)) * 100.0
+
+            if b == self._bitrate:
+                if self._sample_point is not None and s == self._sample_point:
+                    res = (
+                        can_brp,
+                        can_sjw,
+                        can_tseg1,
+                        can_tseg2
+                    )
+                    break
+
+                elif s > sample:
+                    res = (
+                        can_brp,
+                        can_sjw,
+                        can_tseg1,
+                        can_tseg2
+                    )
+                    sample = s
+
+            elif res is None:
+                if close_low < b < self._bitrate:
+                    if (
+                        self._sample_point is None or
+                        (high_sample - self._sample_point > s - self._sample_point)
+                    ):
+                        low_sample = s
+                        close_low = b
+                        low = (
+                            can_brp,
+                            can_sjw,
+                            can_tseg1,
+                            can_tseg2
+                        )
+
+                elif close_high > b > self._bitrate:
+                    if (
+                        self._sample_point is None or
+                        (high_sample - self._sample_point > s - self._sample_point)
+                    ):
+                        high_sample = s
+                        close_high = b
+                        high = (
+                            can_brp,
+                            can_sjw,
+                            can_tseg1,
+                            can_tseg2
+                        )
+        if res is None:
+            if self._sample_point is not None:
+                if low_sample - self._sample_point < high_sample - self._sample_point:
+                    res = low
+                else:
+                    res = high
+
+            elif (
+                    self._bitrate - close_low < close_high - self._bitrate and
+                    low_sample > high_sample
+            ):
+                res = low
+            else:
+                res = high
+
+        self._brp, self._sjw, self._tseg1, self._tseg2 = res
+
     @property
     def nbt(self) -> int:
         """Nominal Bit Time."""
@@ -100,6 +205,9 @@ class BitTiming:
     @property
     def brp(self) -> int:
         """Bit Rate Prescaler."""
+        if self._brp is None and self._bitrate is not None:
+            self._calc_timings()
+
         if self._brp:
             return self._brp
         if self._f_clock and self._bitrate:
@@ -109,6 +217,9 @@ class BitTiming:
     @property
     def sjw(self) -> int:
         """Synchronization Jump Width."""
+        if self._sjw is None and self._bitrate is not None:
+            self._calc_timings()
+
         if not self._sjw:
             raise ValueError("sjw must be specified")
         return self._sjw
@@ -119,6 +230,9 @@ class BitTiming:
 
         The number of quanta from (but not including) the Sync Segment to the sampling point.
         """
+        if self._tseg1 is None and self._bitrate is not None:
+            self._calc_timings()
+
         if not self._tseg1:
             raise ValueError("tseg1 must be specified")
         return self._tseg1
@@ -129,6 +243,9 @@ class BitTiming:
 
         The number of quanta from the sampling point to the end of the bit.
         """
+        if self._tseg2 is None and self._bitrate is not None:
+            self._calc_timings()
+
         if not self._tseg2:
             raise ValueError("tseg2 must be specified")
         return self._tseg2
@@ -154,6 +271,16 @@ class BitTiming:
     def sample_point(self) -> float:
         """Sample point in percent."""
         return 100.0 * (self.nbt - self.tseg2) / self.nbt
+
+    @sample_point.setter
+    def sample_point(self, value: Optional[Union[int, float]]):
+        if self._bitrate is None:
+            raise ValueError('sample point can only be set if there has been a bitrate provided')
+        self._brp = None
+        self._sjw = None
+        self._tseg1 = None
+        self._tseg2 = None
+        self._sample_point = value
 
     @property
     def btr0(self) -> int:
@@ -230,3 +357,12 @@ class BitTiming:
             kwargs["nof_samples"] = self._nof_samples
         args = ", ".join(f"{key}={value}" for key, value in kwargs.items())
         return f"can.BitTiming({args})"
+
+
+if __name__ == '__main__':
+    btiming = BitTiming(500000, 8000000)
+    print(btiming)
+    print()
+
+    btiming = BitTiming(500000, 8000000, sample_point=87.5)
+    print(btiming)
