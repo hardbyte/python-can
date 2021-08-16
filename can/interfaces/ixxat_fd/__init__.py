76de62528ea1ef5f9951a1a55a8d679030e471d7
fjburgos <fjburgos@gmail.com>
2021-08-16 10:49:04 +0200

    Added: Ixxat interface copy before start changing for canfd.

---
+++ can/interfaces/ixxat_fd/__init__.py
@@ -0,0 +1,7 @@
+"""
+Ctypes wrapper module for IXXAT Virtual CAN Interface V3 on win32 systems
+
+Copyright (C) 2016 Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
+"""
+
+from can.interfaces.ixxat.canlib import IXXATBus, get_ixxat_hwids
