b5653cfa3cd52bf16725d04ef3bdb61521561281
fjburgos <fjburgos@gmail.com>
2021-09-13 17:24:58 +0200

    Changed: Merged structures, exceptions and constants from ixxat-fd to ixxat.

--- can/interfaces/ixxat_fd/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Ctypes wrapper module for IXXAT Virtual CAN Interface V3 on win32 systems
-
-Copyright (C) 2016 Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
-"""
-
-from can.interfaces.ixxat_fd.canlib import IXXATBus, get_ixxat_hwids
