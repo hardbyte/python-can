5d4e673678346cb19d81dcdb92ed881d7fd0c310
Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
2021-10-15 11:43:16 +0200

    Now wrapping IXXAT VCI v4

--- can/interfaces/ixxat/__init__.py
+++ can/interfaces/ixxat/__init__.py
@@ -1,7 +1,7 @@
 """
 Ctypes wrapper module for IXXAT Virtual CAN Interface V4 on win32 systems
 
-Copyright (C) 2016 Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
+Copyright (C) 2016-2021 Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
 """
 
 from can.interfaces.ixxat.canlib import IXXATBus
