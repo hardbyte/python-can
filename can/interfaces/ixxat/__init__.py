348001397eed0b9e30d3a942bb14f3e85d35b5d9
Marcel Kanter <marcel.kanter@googlemail.com>
2021-12-03 15:29:18 +0100

    Adapter enumeration for ixxat bus
    Add the enumeration functionality and test it. The enumeration is useful to skip tests which rely on connected hardware.

--- can/interfaces/ixxat/__init__.py
+++ can/interfaces/ixxat/__init__.py
@@ -1,7 +1,8 @@
 """
-Ctypes wrapper module for IXXAT Virtual CAN Interface V3 on win32 systems
+Ctypes wrapper module for IXXAT Virtual CAN Interface on win32 systems
 
 Copyright (C) 2016 Giuseppe Corbelli <giuseppe.corbelli@weightpack.com>
+Copyright (c) 2021 Marcel Kanter <marcel.kanter@googlemail.com>
 """
 
-from can.interfaces.ixxat.canlib import IXXATBus, get_ixxat_hwids
+from can.interfaces.ixxat.canlib import IXXATBus
--- test/test_interface_ixxat.py
+++ test/test_interface_ixxat.py
@@ -1,12 +1,14 @@
 """
 Unittest for ixxat interface.
 
-Run only this test:
-python setup.py test --addopts "--verbose -s test/test_interface_ixxat.py"
+Copyright (c) 2020, 2021 Marcel Kanter <marcel.kanter@googlemail.com>
 """
 
 import unittest
 import can
+import sys
+
+from can.interfaces.ixxat import IXXATBus
 
 
 class SoftwareTestCase(unittest.TestCase):
@@ -15,10 +17,7 @@ class SoftwareTestCase(unittest.TestCase):
     """
 
     def setUp(self):
-        try:
-            bus = can.Bus(interface="ixxat", channel=0)
-            bus.shutdown()
-        except can.CanInterfaceNotImplementedError:
+        if sys.platform != "win32" and sys.platform != "cygwin":
             raise unittest.SkipTest("not available on this platform")
 
     def test_bus_creation(self):
@@ -34,28 +33,48 @@ class SoftwareTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             can.Bus(interface="ixxat", channel=0, txFifoSize=0)
 
+        # non-existent channel (use arbitrary high value)
+        with self.assertRaises(can.CanInitializationError):
+            can.Bus(interface="ixxat", channel=0xFFFF)
+
+    def test_adapter_enumeration(self):
+        # Enumeration of adapters should always work and the result should support len and be iterable
+        adapters = IXXATBus.list_adapters()
+        n = 0
+        for adapter in adapters:
+            n += 1
+        self.assertEqual(len(adapters), n)
+
 
 class HardwareTestCase(unittest.TestCase):
     """
     Test cases that rely on an existing/connected hardware.
+    THEY NEED TO BE EXECUTED WITH AT LEAST ONE CONNECTED ADAPTER!
     """
 
     def setUp(self):
-        try:
-            bus = can.Bus(interface="ixxat", channel=0)
-            bus.shutdown()
-        except can.CanInterfaceNotImplementedError:
+        if sys.platform != "win32" and sys.platform != "cygwin":
             raise unittest.SkipTest("not available on this platform")
 
     def test_bus_creation(self):
-        # non-existent channel -> use arbitrary high value
-        with self.assertRaises(can.CanInitializationError):
-            can.Bus(interface="ixxat", channel=0xFFFF)
+        # Test the enumeration of all adapters by opening and closing each adapter
+        adapters = IXXATBus.list_adapters()
+        for adapter in adapters:
+            bus = can.Bus(interface="ixxat", adapter=adapter)
+            bus.shutdown()
 
     def test_send_after_shutdown(self):
-        with can.Bus(interface="ixxat", channel=0) as bus:
-            with self.assertRaises(can.CanOperationError):
-                bus.send(can.Message(arbitration_id=0x3FF, dlc=0))
+        # At least one adapter is needed, skip the test if none can be found
+        adapters = IXXATBus.list_adapters()
+        if len(adapters) == 0:
+            raise unittest.SkipTest("No adapters found")
+
+        bus = can.Bus(interface="ixxat", channel=0)
+        msg = can.Message(arbitration_id=0x3FF, dlc=0)
+        # Intentionally close the bus now and try to send afterwards. This should lead to an Exception
+        bus.shutdown()
+        with self.assertRaises(can.CanOperationError):
+            bus.send(msg)
 
 
 if __name__ == "__main__":
