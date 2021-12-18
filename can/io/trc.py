d826c069313b934f7a127436c2d3b252d94bffe7
Peter Kessen <p.kessen@kessen-peter.de>
2021-12-18 14:55:25 +0000

    Format code with black

--- can/io/trc.py
+++ can/io/trc.py
@@ -136,7 +136,9 @@ class TRCWriter(BaseIOHandler, Listener):
             self.filepath = "Unknown"
             self._write_line = self._write_line_text
             logger.warning("TRCWriter: Text mode io can result in wrong line endings")
-            logger.debug(f"TRCWriter: Text mode io line ending setting: {file.newlines}")
+            logger.debug(
+                f"TRCWriter: Text mode io line ending setting: {file.newlines}"
+            )
         else:
             self.filepath = "Unknown"
             self._write_line = self._write_line_binary
