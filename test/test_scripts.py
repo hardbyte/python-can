dddbf6b2ac0d52ebd4345520f34f089930e3b432
Peter Kessen <p.kessen@kessen-peter.de>
2021-12-19 16:28:20 +0100

    Add assertions for logger

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -95,6 +95,7 @@ class TestLoggerScript(CanScriptTest):
             arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
         )
 
+        # Patch VirtualBus object
         patcher_virtual_bus = mock.patch("can.interfaces.virtual.VirtualBus", spec=True)
         self.MockVirtualBus = patcher_virtual_bus.start()
         self.addCleanup(patcher_virtual_bus.stop)
@@ -102,12 +103,22 @@ class TestLoggerScript(CanScriptTest):
         mock_virtual_bus.recv = Mock(side_effect=[msg, KeyboardInterrupt])
         mock_virtual_bus.shutdown = Mock()
 
+        # Patch Logger object
+        patcher_logger = mock.patch("can.Logger", spec=True)
+        self.MockLogger = patcher_logger.start()
+        self.addCleanup(patcher_logger.stop)
+        mock_logger = self.MockLogger.return_value
+        mock_logger.stop = Mock()
+
         module = self._import()
 
         sys.argv = [sys.argv[0], "-i", "virtual"]
         module.main()
         self.MockVirtualBus.assert_called_once()
         mock_virtual_bus.shutdown.assert_called_once()
+
+        self.MockLogger.assert_called_once()
+        mock_logger.stop.assert_called_once()
 
 
 class TestPlayerScript(CanScriptTest):
