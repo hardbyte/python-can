0362acbcae9dcdcb97474cc97788fd1983184b84
Peter Kessen <p.kessen@kessen-peter.de>
2021-12-20 19:05:51 +0100

    Add test case for player.py

---
+++ test/test_player.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+This module tests the functions inside of player.py
+"""
+
+import unittest
+from unittest import mock
+from unittest.mock import Mock
+import os
+import sys
+import can
+import can.player
+
+from .config import *
+
+
+class TestPlayerScriptModule(unittest.TestCase):
+    def setUp(self) -> None:
+        # Patch VirtualBus object
+        patcher_virtual_bus = mock.patch("can.interfaces.virtual.VirtualBus", spec=True)
+        self.MockVirtualBus = patcher_virtual_bus.start()
+        self.addCleanup(patcher_virtual_bus.stop)
+        self.mock_virtual_bus = self.MockVirtualBus.return_value
+        self.mock_virtual_bus.shutdown = Mock()
+
+        self.baseargs = [sys.argv[0], "-i", "virtual"]
+        self.logfile = os.path.join(os.path.dirname(__file__), "data", "test_CanMessage.asc")
+
+    def assertSuccessfullCleanup(self):
+        self.MockVirtualBus.assert_called_once()
+
+    def test_play_virtual(self):
+        sys.argv = self.baseargs + [self.logfile]
+        can.player.main()
+        self.assertSuccessfullCleanup()
+
+
+if __name__ == "__main__":
+    unittest.main()
