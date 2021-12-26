90d2769870b3893f815df5e83803f9f5765110d8
Marcin Bober <mbober1@gmail.com>
2021-12-26 12:51:17 +0100

    proper tests

--- test/test_cfuc.py
+++ test/test_cfuc.py
@@ -47,62 +47,57 @@ class CFUCTestCase(unittest.TestCase):
         Tests the transfer of init frame
         """
         self.bus = can.Bus(bustype="cfuc", channel="loop://", CANBaudRate=250000, IsFD=True, FDDataBaudRate=500000)
-        self.serial = self.bus.ser
-        msg = self.serial.read(self.serial.in_waiting)
-        # msg = self.bus.recv(0)
+        msg = self.bus.recv(None)
         self.assertIsNotNone(msg)
+
 
     def test_tx_frame_extended(self):
         """
         Tests the transfer of extented CAN frame
         """
         self.bus = can.Bus(bustype="cfuc", channel="loop://", CANBaudRate=250000, IsFD=True, FDDataBaudRate=500000)
-        self.serial = self.bus.ser
-        self.serial.read(self.serial.in_waiting)
+        msg = self.bus.recv(None)
 
         msg = can.Message(
             arbitration_id=0x12ABCDEF, is_extended_id=True, data=[0xAA, 0x55]
         )
         self.bus.send(msg)
 
-        data = self.serial.read(self.serial.in_waiting)
-        self.assertEqual(data[11],64)  # standart / extended field 
-        # self.assertIsNotNone(data)
+        msg = self.bus.recv(None)
+        self.assertTrue(msg.is_extended_id)  # standart / extended field 
 
     
     def test_tx_frame_standart(self):
         """
-        Tests the transfer of extented CAN frame
+        Tests the transfer of standart CAN frame
         """
         self.bus = can.Bus(bustype="cfuc", channel="loop://", CANBaudRate=250000, IsFD=True, FDDataBaudRate=500000)
-        self.serial = self.bus.ser
-        self.serial.read(self.serial.in_waiting)
+        msg = self.bus.recv(None)
 
         msg = can.Message(
             arbitration_id=0x12ABCDEF, is_extended_id=False, data=[0xAA, 0x55]
         )
         self.bus.send(msg)
 
-        data = self.serial.read(self.serial.in_waiting) 
-
-        self.assertEqual(data[11],0)  # standart extended 
-        # self.assertEqual(data[18],2)          
+        msg = self.bus.recv(None)
+        self.assertFalse(msg.is_extended_id)  # standart / extended field      
     
+
     def test_tx_fd_frame(self):
         """
-        Tests the transfer of extented CAN frame
+        Tests the transfer of Flexible Data-Rate CAN frame
         """
         self.bus = can.Bus(bustype="cfuc", channel="loop://", CANBaudRate=250000, IsFD=True, FDDataBaudRate=500000)
-        self.serial = self.bus.ser
-        self.serial.read(self.serial.in_waiting)
+        msg = self.bus.recv(None)
 
         msg = can.Message(
             arbitration_id=0x12ABCDEF, is_extended_id=True, is_fd = True, data=[0xAA, 0x55]
         )
         self.bus.send(msg)
 
-        data = self.serial.read(self.serial.in_waiting)
-        self.assertIsNotNone(data)
+        msg = self.bus.recv(None)
+        self.assertTrue(msg.is_extended_id)
+        self.assertTrue(msg.is_fd)
                                                                                    #len           #ErrorState    #BitRateSwitch     #FDFormat
 #        b'\x02\x00\x00\x00\xef\xcd\xab\x12\x00\x00\x00@\x00 \x00\x00\x00\x00\ x00\x02\x00\x00\ x00\x00\x00\x00 \x00\x00\x00\x00\ x00\x00\x00\x00\ x00\x00\x00\x00\ x00\x00\x00\xaaU\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
     #    b'\x02\x00\x00\x00\xef\xcd\xab\x12\x00\x00\x00 \x00 \x00\x00\x00\x00\ x00\x00\x02\x00\ x00\x00\x00\x00 \x00\x00\x00\x00\ x00\x00\x00\x00\ x00\x00\x00\x00\ x00\x00\x00\x00\xaaU\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
