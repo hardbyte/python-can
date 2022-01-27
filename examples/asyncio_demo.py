b250424b0c9ffdfd886805aa0b16cf93c5881279
Felix Divo <4403130+felixdivo@users.noreply.github.com>
2022-01-27 08:13:18 +0100

    Modernize example

--- examples/asyncio_demo.py
+++ examples/asyncio_demo.py
@@ -8,54 +8,45 @@ import asyncio
 import can
 
 
-def print_message(msg):
+def print_message(msg: can.Message) -> None:
     """Regular callback function. Can also be a coroutine."""
     print(msg)
 
 
-async def main():
+async def main() -> None:
     """The main function that runs in the loop."""
 
-    bus = can.Bus("vcan0", bustype="virtual", receive_own_messages=True)
-    reader = can.AsyncBufferedReader()
-    logger = can.Logger("logfile.asc")
+    with can.Bus("vcan0", bustype="virtual", receive_own_messages=True) as bus:
+        reader = can.AsyncBufferedReader()
+        logger = can.Logger("logfile.asc")
 
-    listeners = [
-        print_message,  # Callback function
-        reader,  # AsyncBufferedReader() listener
-        logger,  # Regular Listener object
-    ]
-    # Create Notifier with an explicit loop to use for scheduling of callbacks
-    loop = asyncio.get_event_loop()
-    notifier = can.Notifier(bus, listeners, loop=loop)
-    # Start sending first message
-    bus.send(can.Message(arbitration_id=0))
+        listeners = [
+            print_message,  # Callback function
+            reader,  # AsyncBufferedReader() listener
+            logger,  # Regular Listener object
+        ]
+        # Create Notifier with an explicit loop to use for scheduling of callbacks
+        loop = asyncio.get_running_loop()
+        notifier = can.Notifier(bus, listeners, loop=loop)
+        # Start sending first message
+        bus.send(can.Message(arbitration_id=0))
 
-    print("Bouncing 10 messages...")
-    for _ in range(10):
-        # Wait for next message from AsyncBufferedReader
-        msg = await reader.get_message()
-        # Delay response
-        await asyncio.sleep(0.5)
-        msg.arbitration_id += 1
-        bus.send(msg)
-    # Wait for last message to arrive
-    await reader.get_message()
-    print("Done!")
+        print("Bouncing 10 messages...")
+        for _ in range(10):
+            # Wait for next message from AsyncBufferedReader
+            msg = await reader.get_message()
+            # Delay response
+            await asyncio.sleep(0.5)
+            msg.arbitration_id += 1
+            bus.send(msg)
 
-    # Clean-up
-    notifier.stop()
-    bus.shutdown()
+        # Wait for last message to arrive
+        await reader.get_message()
+        print("Done!")
+
+        # Clean-up
+        notifier.stop()
 
 
 if __name__ == "__main__":
-    try:
-        # Get the default event loop
-        LOOP = asyncio.get_event_loop()
-        # Run until main coroutine finishes
-        LOOP.run_until_complete(main())
-    finally:
-        LOOP.close()
-
-    # or on Python 3.7+ simply
-    # asyncio.run(main())
+    asyncio.run(main())
