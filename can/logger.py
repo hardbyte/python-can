14301543424a490d1607b2d0ed56c059cdff3d09
Jack Cook <jack-c-cook@protonmail.com>
2022-06-10 02:28:06 -0500

    Enhance `can.logger` to consider the `append` option (#1327)
    * Add passage of **options to the logger initialization
    
    * Add -a boolean option to the parser
    
    * Add manual arg parse for boolean variables
    
    This solution is only required until Python>=3.9.
    
    * Fix the index-error check
    
    * Bolster help documentation for new -a option
    
    * Clean up variable names, notes, etc.
    
    * Whitespace formatting (PEP-8 style)
    
    * Make formatting recomendations based on `pylint logger.py`
    
    * Format code with black logger.py to stop failing the `black` check
    
    * Reduce complexity of append arg parse incorporation
    
    * Fix format with `black can/logger.py`
    
    * Change append argparse access from `results.a` to `results.append`
    
    * Add `dest` option to append argument

--- can/logger.py
+++ can/logger.py
@@ -31,9 +31,10 @@ def _create_base_argument_parser(parser: argparse.ArgumentParser) -> None:
     parser.add_argument(
         "-c",
         "--channel",
-        help='''Most backend interfaces require some sort of channel.
-    For example with the serial interface the channel might be a rfcomm device: "/dev/rfcomm0"
-    With the socketcan interfaces valid channel examples include: "can0", "vcan0"''',
+        help=r"Most backend interfaces require some sort of channel. For "
+        r"example with the serial interface the channel might be a rfcomm"
+        r' device: "/dev/rfcomm0". With the socketcan interface valid '
+        r'channel examples include: "can0", "vcan0".',
     )
 
     parser.add_argument(
@@ -60,11 +61,10 @@ def _create_base_argument_parser(parser: argparse.ArgumentParser) -> None:
     parser.add_argument(
         "extra_args",
         nargs=argparse.REMAINDER,
-        help="""\
-        The remaining arguments will be used for the interface initialisation.
-        For example, `-i vector -c 1 --app-name=MyCanApp` is the equivalent to 
-        opening the bus with `Bus('vector', channel=1, app_name='MyCanApp')`
-        """,
+        help=r"The remaining arguments will be used for the interface "
+        r"initialisation. For example, `-i vector -c 1 --app-name="
+        r"MyCanApp` is the equivalent to opening the bus with `Bus("
+        r"'vector', channel=1, app_name='MyCanApp')",
     )
 
 
@@ -82,8 +82,10 @@ def _append_filter_argument(
         *args,
         "--filter",
         help="R|Space separated CAN filters for the given CAN interface:"
-        "\n      <can_id>:<can_mask> (matches when <received_can_id> & mask == can_id & mask)"
-        "\n      <can_id>~<can_mask> (matches when <received_can_id> & mask != can_id & mask)"
+        "\n      <can_id>:<can_mask> (matches when <received_can_id> & mask =="
+        " can_id & mask)"
+        "\n      <can_id>~<can_mask> (matches when <received_can_id> & mask !="
+        " can_id & mask)"
         "\nFx to show only frames with ID 0x100 to 0x103 and 0x200 to 0x20F:"
         "\n      python -m can.viewer -f 100:7FC 200:7F0"
         "\nNote that the ID and mask are always interpreted as hex values",
@@ -141,7 +143,8 @@ def _parse_additonal_config(unknown_args):
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        description="Log CAN traffic, printing messages to stdout or to a given file.",
+        description="Log CAN traffic, printing messages to stdout or to a "
+        "given file.",
     )
 
     _create_base_argument_parser(parser)
@@ -155,11 +158,20 @@ def main() -> None:
     )
 
     parser.add_argument(
+        "-a",
+        "--append",
+        dest="append",
+        help="Append to the log file if it already exists.",
+        action="store_true",
+    )
+
+    parser.add_argument(
         "-s",
         "--file_size",
         dest="file_size",
         type=int,
-        help="Maximum file size in bytes. Rotate log file when size threshold is reached.",
+        help="Maximum file size in bytes. Rotate log file when size threshold "
+        "is reached.",
         default=None,
     )
 
@@ -201,12 +213,13 @@ def main() -> None:
     print(f"Connected to {bus.__class__.__name__}: {bus.channel_info}")
     print(f"Can Logger (Started on {datetime.now()})")
 
+    options = {"append": results.append}
     if results.file_size:
         logger = SizedRotatingLogger(
-            base_filename=results.log_file, max_bytes=results.file_size
+            base_filename=results.log_file, max_bytes=results.file_size, **options
         )
     else:
-        logger = Logger(filename=results.log_file)  # type: ignore
+        logger = Logger(filename=results.log_file, **options)  # type: ignore
 
     try:
         while True:
