c0d922496f976cc8014cb63d7538fa516a5f4ff3
Felix Divo <felix.divo@gmail.com>
2022-11-24 13:03:41 +0100

    Add missing type hint

--- can/util.py
+++ can/util.py
@@ -61,7 +61,7 @@ def load_file_config(
     else:
         config.read(path)
 
-    _config = {}
+    _config: Dict[str, str] = {}
 
     if config.has_section(section):
         _config.update(config.items(section))
