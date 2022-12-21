e2071431ab544a36c19f3c12b35033e87da8b68d
felixdivo <felixdivo@users.noreply.github.com>
2022-12-21 15:38:08 +0000

    Format code with black

--- can/interfaces/__init__.py
+++ can/interfaces/__init__.py
@@ -6,31 +6,34 @@ import sys
 from typing import Dict, Tuple
 
 # interface_name => (module, classname)
-BACKENDS: Dict[str, Tuple[str, str]] = cast(Dict[str, Tuple[str, str]], {
-    "kvaser": ("can.interfaces.kvaser", "KvaserBus"),
-    "socketcan": ("can.interfaces.socketcan", "SocketcanBus"),
-    "serial": ("can.interfaces.serial.serial_can", "SerialBus"),
-    "pcan": ("can.interfaces.pcan", "PcanBus"),
-    "usb2can": ("can.interfaces.usb2can", "Usb2canBus"),
-    "ixxat": ("can.interfaces.ixxat", "IXXATBus"),
-    "nican": ("can.interfaces.nican", "NicanBus"),
-    "iscan": ("can.interfaces.iscan", "IscanBus"),
-    "virtual": ("can.interfaces.virtual", "VirtualBus"),
-    "udp_multicast": ("can.interfaces.udp_multicast", "UdpMulticastBus"),
-    "neovi": ("can.interfaces.ics_neovi", "NeoViBus"),
-    "vector": ("can.interfaces.vector", "VectorBus"),
-    "slcan": ("can.interfaces.slcan", "slcanBus"),
-    "robotell": ("can.interfaces.robotell", "robotellBus"),
-    "canalystii": ("can.interfaces.canalystii", "CANalystIIBus"),
-    "systec": ("can.interfaces.systec", "UcanBus"),
-    "seeedstudio": ("can.interfaces.seeedstudio", "SeeedBus"),
-    "cantact": ("can.interfaces.cantact", "CantactBus"),
-    "gs_usb": ("can.interfaces.gs_usb", "GsUsbBus"),
-    "nixnet": ("can.interfaces.nixnet", "NiXNETcanBus"),
-    "neousys": ("can.interfaces.neousys", "NeousysBus"),
-    "etas": ("can.interfaces.etas", "EtasBus"),
-    "socketcand": ("can.interfaces.socketcand", "SocketCanDaemonBus"),
-})
+BACKENDS: Dict[str, Tuple[str, str]] = cast(
+    Dict[str, Tuple[str, str]],
+    {
+        "kvaser": ("can.interfaces.kvaser", "KvaserBus"),
+        "socketcan": ("can.interfaces.socketcan", "SocketcanBus"),
+        "serial": ("can.interfaces.serial.serial_can", "SerialBus"),
+        "pcan": ("can.interfaces.pcan", "PcanBus"),
+        "usb2can": ("can.interfaces.usb2can", "Usb2canBus"),
+        "ixxat": ("can.interfaces.ixxat", "IXXATBus"),
+        "nican": ("can.interfaces.nican", "NicanBus"),
+        "iscan": ("can.interfaces.iscan", "IscanBus"),
+        "virtual": ("can.interfaces.virtual", "VirtualBus"),
+        "udp_multicast": ("can.interfaces.udp_multicast", "UdpMulticastBus"),
+        "neovi": ("can.interfaces.ics_neovi", "NeoViBus"),
+        "vector": ("can.interfaces.vector", "VectorBus"),
+        "slcan": ("can.interfaces.slcan", "slcanBus"),
+        "robotell": ("can.interfaces.robotell", "robotellBus"),
+        "canalystii": ("can.interfaces.canalystii", "CANalystIIBus"),
+        "systec": ("can.interfaces.systec", "UcanBus"),
+        "seeedstudio": ("can.interfaces.seeedstudio", "SeeedBus"),
+        "cantact": ("can.interfaces.cantact", "CantactBus"),
+        "gs_usb": ("can.interfaces.gs_usb", "GsUsbBus"),
+        "nixnet": ("can.interfaces.nixnet", "NiXNETcanBus"),
+        "neousys": ("can.interfaces.neousys", "NeousysBus"),
+        "etas": ("can.interfaces.etas", "EtasBus"),
+        "socketcand": ("can.interfaces.socketcand", "SocketCanDaemonBus"),
+    },
+)
 
 if sys.version_info >= (3, 8):
     from importlib.metadata import entry_points
