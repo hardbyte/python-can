18b86bab92dd03e1f8d775e9cd5cacdad5fe2be5
Felix Divo <4403130+felixdivo@users.noreply.github.com>
2022-12-21 18:34:28 +0100

    Update can/interfaces/__init__.py
    Co-authored-by: zariiii9003 <52598363+zariiii9003@users.noreply.github.com>

--- can/interfaces/__init__.py
+++ can/interfaces/__init__.py
@@ -48,7 +48,7 @@ if sys.version_info >= (3, 8):
         BACKENDS.update(
             {
                 # This cast in wrong if interface.value is formatted badly, but we just fail later
-                interface.name: cast(Tuple[str, str], tuple(interface.value.split(":")))
+                interface.name: cast(Tuple[str, str], tuple(interface.value.split(":", maxsplit=1)))
                 for interface in entry_points().get("can.interface", [])
             }
         )
