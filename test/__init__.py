e3f2f6e326762f47744b2c0494f430e30f9aa11f
TJ Bruno <tj.bruno@everactive.com>
2023-01-25 13:24:40 -0800

    Add warning filter

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+
+import pytest
+
+# Ignore exceptions being raised as a result of BusABC.__del__
+pytestmark = pytest.mark.filterwarnings(
+    "ignore::pytest.PytestUnraisableExceptionWarning"
+)
