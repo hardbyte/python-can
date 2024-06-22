0de8ca82f1c18afff438a7c0efba5f512ea6581e
BroderickJack <39026705+BroderickJack@users.noreply.github.com>
2024-06-22 07:03:11 -0400

    Support CANdapter extended length arbitration ID (#1528)
    * Support CANdapter extended length arbitration ID
    
    * Update can/interfaces/slcan.py
    
    More efficient check of arbitration ID
    
    Co-authored-by: zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
    
    * Add description of x extended arbitration identifier
    
    * Add test for CANDapter extended arbitration ID
    
    * Update test/test_slcan.py
    
    Type fix in slcan test
    
    Co-authored-by: zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
    
    * format black
    
    ---------
    
    Co-authored-by: zariiii9003 <52598363+zariiii9003@users.noreply.github.com>

--- can/interfaces/slcan.py
+++ can/interfaces/slcan.py
@@ -215,7 +215,10 @@ class slcanBus(BusABC):
 
         if not string:
             pass
-        elif string[0] == "T":
+        elif string[0] in (
+            "T",
+            "x",  # x is an alternative extended message identifier for CANDapter
+        ):
             # extended frame
             canId = int(string[1:9], 16)
             dlc = int(string[9])
