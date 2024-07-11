acc90c6072d7ece0a4afbab2b9dd3d1ba2857275
zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
2024-07-11 09:00:43 +0200

    Stop notifier in examples (#1814)

--- examples/print_notifier.py
+++ examples/print_notifier.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
 import time
+
 import can
 
 
 def main():
-    with can.Bus(receive_own_messages=True) as bus:
+    with can.Bus(interface="virtual", receive_own_messages=True) as bus:
         print_listener = can.Printer()
-        can.Notifier(bus, [print_listener])
+        notifier = can.Notifier(bus, [print_listener])
 
         bus.send(can.Message(arbitration_id=1, is_extended_id=True))
         bus.send(can.Message(arbitration_id=2, is_extended_id=True))
         bus.send(can.Message(arbitration_id=1, is_extended_id=False))
 
         time.sleep(1.0)
+        notifier.stop()
 
 
 if __name__ == "__main__":
