991aa05a1f556f2ddb7c6b1f58bb1f189a17f92a
Peter Kessen <p.kessen@kessen-peter.de>
2025-02-23 20:16:02 +0100

    Add very basic test

--- test/test_bridge.py
+++ test/test_bridge.py
@@ -4,6 +4,7 @@
 This module tests the functions inside of bridge.py
 """
 
+import sys
 import unittest
 from unittest import mock
 from unittest.mock import Mock
@@ -21,13 +22,26 @@ class TestBridgeScriptModule(unittest.TestCase):
         self.mock_virtual_bus = self.MockVirtualBus.return_value
         self.mock_virtual_bus.shutdown = Mock()
 
+        # Patch time sleep object
+        patcher_sleep = mock.patch("can.io.player.time.sleep", spec=True)
+        self.MockSleep = patcher_sleep.start()
+        self.addCleanup(patcher_sleep.stop)
+
         self.testmsg = can.Message(
             arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
         )
 
+        self.busargs = ["-i", "virtual"]
+
     def assert_successfull_cleanup(self):
-        self.MockVirtualBus.assert_called_once()
-        self.mock_virtual_bus.shutdown.assert_called_once()
+        self.MockVirtualBus.assert_called()
+
+    def test_bridge_no_config(self):
+        self.MockSleep.side_effect = KeyboardInterrupt
+        sys.argv = [sys.argv[0], *self.busargs, "--", *self.busargs]
+        can.bridge.main()
+
+        self.assert_successfull_cleanup()
 
 
 if __name__ == "__main__":
