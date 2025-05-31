f43bedbc68777162132251bad91aee6ed77d6b8b
Joachim Stolberg <106076945+HMS-jost@users.noreply.github.com>
2025-05-31 21:55:54 +0200

    Handle timer overflow message and build timestamp according to the epoch in IXXATBus (#1934)
    * Handle timer overflow message and build timestamp according to the epoch
    
    * Revert "Handle timer overflow message and build timestamp according to the epoch"
    
    This reverts commit e0ccfb0d4b3fef9fbed24c9a6e673e5199e35c52.
    
    * Handle timer overflow message and build timestamp according to the epoch
    
    * Fix formating issues
    
    * Format with black

--- can/interfaces/ixxat/canlib_vcinpl.py
+++ can/interfaces/ixxat/canlib_vcinpl.py
@@ -13,6 +13,7 @@ import ctypes
 import functools
 import logging
 import sys
+import time
 import warnings
 from collections.abc import Sequence
 from typing import Callable, Optional, Union
@@ -620,7 +621,15 @@ class IXXATBus(BusABC):
                 log.info("Accepting ID: 0x%X MASK: 0x%X", code, mask)
 
         # Start the CAN controller. Messages will be forwarded to the channel
+        start_begin = time.time()
         _canlib.canControlStart(self._control_handle, constants.TRUE)
+        start_end = time.time()
+
+        # Calculate an offset to make them relative to epoch
+        # Assume that the time offset is in the middle of the start command
+        self._timeoffset = start_begin + (start_end - start_begin / 2)
+        self._overrunticks = 0
+        self._starttickoffset = 0
 
         # For cyclic transmit list. Set when .send_periodic() is first called
         self._scheduler = None
@@ -693,6 +702,9 @@ class IXXATBus(BusABC):
                         f"Unknown CAN info message code {self._message.abData[0]}",
                     )
                 )
+                # Handle CAN start info message
+                if self._message.abData[0] == constants.CAN_INFO_START:
+                    self._starttickoffset = self._message.dwTime
             elif self._message.uMsgInfo.Bits.type == constants.CAN_MSGTYPE_ERROR:
                 if self._message.uMsgInfo.Bytes.bFlags & constants.CAN_MSGFLAGS_OVR:
                     log.warning("CAN error: data overrun")
@@ -709,7 +721,8 @@ class IXXATBus(BusABC):
                         self._message.uMsgInfo.Bytes.bFlags,
                     )
             elif self._message.uMsgInfo.Bits.type == constants.CAN_MSGTYPE_TIMEOVR:
-                pass
+                # Add the number of timestamp overruns to the high word
+                self._overrunticks += self._message.dwMsgId << 32
             else:
                 log.warning(
                     "Unexpected message info type 0x%X",
@@ -741,11 +754,12 @@ class IXXATBus(BusABC):
             # Timed out / can message type is not DATA
             return None, True
 
-        # The _message.dwTime is a 32bit tick value and will overrun,
-        # so expect to see the value restarting from 0
         rx_msg = Message(
-            timestamp=self._message.dwTime
-            / self._tick_resolution,  # Relative time in s
+            timestamp=(
+                (self._message.dwTime + self._overrunticks - self._starttickoffset)
+                / self._tick_resolution
+            )
+            + self._timeoffset,
             is_remote_frame=bool(self._message.uMsgInfo.Bits.rtr),
             is_extended_id=bool(self._message.uMsgInfo.Bits.ext),
             arbitration_id=self._message.dwMsgId,
