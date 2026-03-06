ceeef98bc9424782251e2169d09e0fb4f1df89a6
Ben Gardiner <ben.l.gardiner@gmail.com>
2026-03-05 22:55:10 -0500

    gs_usb: explicitly handle timeout=None separate from =0 (@zariiii9003)

--- can/interfaces/gs_usb.py
+++ can/interfaces/gs_usb.py
@@ -178,10 +178,13 @@ class GsUsbBus(can.BusABC):
         """
         frame = GsUsbFrame()
 
-        # Do not set timeout as None or zero here to avoid blocking
-        timeout_ms = round(timeout * 1000) if timeout else 0
-        if not self.gs_usb.read(frame=frame, timeout_ms=timeout_ms):
-            return None, False
+        if timeout is None:
+            timeout_ms = 0
+        else:
+            # Do not set timeout as None or zero here to avoid blocking
+            timeout_ms = round(timeout * 1000) if timeout else 1
+            if not self.gs_usb.read(frame=frame, timeout_ms=timeout_ms):
+                return None, False
 
         msg = can.Message(
             timestamp=frame.timestamp,
